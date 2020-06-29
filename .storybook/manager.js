b9894aad17131591b55c48f7461fa16365f40265
Baptiste Macaire <baptiste.mac@gmail.com>
2020-06-29 15:54:59 -0400

    add server to expose twig files

--- .storybook/manager.js
+++ .storybook/manager.js
@@ -1,5 +1,12 @@
 import { addons } from '@storybook/addons';
+import { create } from '@storybook/theming/create';
 
 addons.setConfig({
   showRoots: true,
+  theme: create({
+    base: 'light',
+    brandTitle: 'Bifrost',
+    brandUrl: 'https://videotron.com/',
+    brandImage: '../projects/front-end-library/src/assets/bifrost-logo.png',
+  })
 });
---
+++ server/server.js
@@ -0,0 +1,17 @@
+  const app = require('express')();
+  const port = process.env.NODE_PORT || 3001;
+  const {TwingEnvironment, TwingLoaderFilesystem} = require('twing');
+  let loader = new TwingLoaderFilesystem('./projects/front-end-library/src/utils/');
+  let twing = new TwingEnvironment(loader);
+
+  app.get('/:component', function (req, res) {
+    
+    Object.assign(req.params, req.query);
+    twing.render('main.twig', req.params).then((output) => {
+        res.end(output);
+    });
+  });
+
+  app.listen(port, () => {
+    console.log('Node.js Express server listening on port '+port);
+  });
