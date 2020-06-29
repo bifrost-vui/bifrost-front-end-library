0728187261778d1362b1bae49fd35a809c480009
Baptiste Macaire <baptiste.mac@gmail.com>
2020-06-29 14:49:20 -0400

    colors

---
+++ projects/front-end-library/src/lib/components/button/button2.stories.ts
@@ -0,0 +1,18 @@
+import { ButtonComponent } from './angular/button.component';
+
+export default {
+  title: 'Components/Controls',
+  component: ButtonComponent,
+  parameters: { docs: { iframeHeight: 120 } },
+};
+
+const ButtonStory = (args) => ({
+  component: ButtonComponent,
+  props: args,
+});
+
+export const Basic = ButtonStory.bind({});
+Basic.args = { label: 'Args test', disabled: false };
+
+export const Disabled = ButtonStory.bind({});
+Disabled.args = { label: 'Disabled', disabled: true };
