f1900744aae3444ac30b6804a9c079507666bcc2
Baptiste Macaire <baptiste.mac@gmail.com>
2020-07-07 23:37:46 -0400

    serverless function 3

--- api/hello.js
+++ api/hello.js
@@ -1,8 +1,11 @@
 module.exports = (req, res) => {
     const {TwingEnvironment, TwingLoaderFilesystem} = require('twing');
-    let loader = new TwingLoaderFilesystem('./projects/front-end-library/src/utils/');
+    let loader = new TwingLoaderFilesystem('../projects/front-end-library/src/utils/');
     let twing = new TwingEnvironment(loader);
 
-    const { name = 'World' } = req.query
-    res.status(200).send(`Yo ${name}!`)
+    // const { name = 'World' } = req.query
+    // res.status(200).send(`Yo ${name}!`)
+    twing.render('main.twig', req.query).then((output) => {
+        res.status(200).send(output);
+    });
 }
