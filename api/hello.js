e07372e585c9fce44a4c4c4be9b1bce83b9e26e1
Baptiste Macaire <baptiste.mac@gmail.com>
2020-07-08 00:13:40 -0400

    functions 2

--- api/hello.js
+++ api/hello.js
@@ -1,18 +1,13 @@
 module.exports = (req, res) => {
-    
-    console.log(req, res);
-
     const app = require('express')();
     const path = require('path');
-    //   const port = process.env.NODE_PORT || 3001;
-    const port = 3001;
     const { TwingEnvironment, TwingLoaderFilesystem } = require('twing');
+
     let loader = new TwingLoaderFilesystem(path.join(__dirname, '../projects/front-end-library/src/utils/'));
     let twing = new TwingEnvironment(loader);
 
-    // const { name = 'World' } = req.query
-    // res.status(200).send(`Yo ${name}!`)
-    twing.render('main.twig', req.query).then((output) => {
+    Object.assign(req.params, req.query, { component: 'button' });
+    twing.render('main.twig', req.params).then((output) => {
         res.end(output);
     });
 }
