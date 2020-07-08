16581ce3c54d24fa2de91643bafca963a1230b29
Baptiste Macaire <baptiste.mac@gmail.com>
2020-07-08 00:16:46 -0400

    functions 2D3

--- api/hello.js
+++ api/hello.js
@@ -6,8 +6,10 @@ module.exports = (req, res) => {
     let loader = new TwingLoaderFilesystem(path.join(__dirname, '../projects/front-end-library/src/utils/'));
     let twing = new TwingEnvironment(loader);
 
-    Object.assign(req.params, req.query, { component: 'button' });
-    twing.render('main.twig', req.params).then((output) => {
+    console.log(req);
+
+    // Object.assign(req.params, req.query, { component: 'button' });
+    twing.render('main.twig', req.query).then((output) => {
         res.end(output);
     });
 }
