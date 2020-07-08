782d947bf7547728825a669add707cabbfaf646e
Baptiste Macaire <baptiste.mac@gmail.com>
2020-07-08 09:01:43 -0400

    function 10

--- api/hello.js
+++ api/hello.js
@@ -4,13 +4,13 @@ module.exports = (req, res) => {
     const { TwingEnvironment, TwingLoaderFilesystem } = require('twing');
 
     // let loader = new TwingLoaderFilesystem(path.join(__dirname, '../projects/front-end-library/src/utils/'));
-    let loader = new TwingLoaderFilesystem(path.join(__dirname, ''));
+    let loader = new TwingLoaderFilesystem(path.join(__dirname, './'));
     let twing = new TwingEnvironment(loader);
 
     console.log(req);
 
     // Object.assign(req.params, req.query, { component: 'button' });
-    twing.render('./main.twig', req.query).then((output) => {
+    twing.render('main.twig', req.query).then((output) => {
         res.end(output);
     });
 }
