5d33084717acaec88d48cd1d04d2fa93f410ca80
Baptiste Macaire <baptiste.mac@gmail.com>
2020-07-15 22:47:35 -0400

    serverless

--- server/server.js
+++ server/server.js
@@ -2,7 +2,7 @@
 //   const port = process.env.NODE_PORT || 3001;
   const port = 3001;
   const {TwingEnvironment, TwingLoaderFilesystem} = require('twing');
-  let loader = new TwingLoaderFilesystem('./projects/front-end-library/src/utils/');
+  let loader = new TwingLoaderFilesystem('./projects/front-end-library/src/lib/components/');
   let twing = new TwingEnvironment(loader);
 
   app.get('/:component', function (req, res) {
