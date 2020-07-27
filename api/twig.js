24bcc8172ff5a4ff44faeb16fbfc6401fe770148
Baptiste Macaire <baptiste.mac@gmail.com>
2020-07-27 10:32:01 -0400

    function

--- api/twig.js
+++ api/twig.js
@@ -22,9 +22,7 @@ module.exports = (req, res) => {
     app.set('views', path.join(__dirname, '../projects/front-end-library/src/lib'));
     app.set('view engine', 'twig');
 
-    app.render('main.twig', req.query, function(err, html){ 
-        return console.log((err) ? err : html);
-    });
+    app.render('main.twig', req.query);
 
     // app.get('/', function(req, res){
     //     // console.log(req.params, req);
