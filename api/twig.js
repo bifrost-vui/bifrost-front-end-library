2650347edf0134f37446877437d275d4c45c0578
Baptiste Macaire <baptiste.mac@gmail.com>
2020-07-27 09:49:05 -0400

    function

--- api/twig.js
+++ api/twig.js
@@ -3,7 +3,7 @@ module.exports = (req, res) => {
         express = require('express'),
         app = express();
 
-    const port = 3001;
+    // const port = 3001;
 
     // This section is optional and used to configure twig.
     app.set("twig options", {
@@ -20,14 +20,18 @@ module.exports = (req, res) => {
     app.set('views', 'projects/front-end-library/src/lib');
     app.set('view engine', 'twig');
 
-    app.get('/', function(req, res){
-        // console.log(req.params, req);
-        // Object.assign(req.params, req.query);
-        res.render('main.twig', req.query);
+    app.render('main.twig', req.query, function(err, html){ 
+        return console.log((err) ? err : html);
     });
 
-    Twig.extendFilter('boolean', function(value, params) {
-        // console.log('value', value, params);
-        return (value === 0 || value === '0' || value === 'false') ? false : !!value;
-    });
+    // app.get('/', function(req, res){
+    //     // console.log(req.params, req);
+    //     // Object.assign(req.params, req.query);
+    //     res.render('main.twig', req.query);
+    // });
+
+    // Twig.extendFilter('boolean', function(value, params) {
+    //     // console.log('value', value, params);
+    //     return (value === 0 || value === '0' || value === 'false') ? false : !!value;
+    // });
 }
