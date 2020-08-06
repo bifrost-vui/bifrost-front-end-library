d5d06fbccb2ae986d079e11dffce125bbb754fe8
Baptiste Macaire <baptiste.mac@gmail.com>
2020-08-05 23:12:28 -0400

    add accordion and twig props validation

---
+++ projects/front-end-library/src/lib/components/accordion/angular/accordion.component.ts
@@ -0,0 +1,30 @@
+import { Component, ViewEncapsulation, OnInit, Input } from "@angular/core";
+
+/**
+ * Description of the component Accordion
+ *
+ */
+ 
+@Component({
+  selector: "bf-accordion",
+  templateUrl: './accordion.component.html',
+  styleUrls: ['../common/style.scss'],
+  encapsulation: ViewEncapsulation.None
+})
+export class AccordionComponent implements OnInit {
+    constructor() {}
+
+    /**
+    * The inner text.
+    *
+    * @required
+    */
+    @Input() label: string;
+
+    /** Class override */
+    @Input() styleClass: string;
+
+    ngOnInit() {
+        console.log('Accordion', this);
+    }
+}
