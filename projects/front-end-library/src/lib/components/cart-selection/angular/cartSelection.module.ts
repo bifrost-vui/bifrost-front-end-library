670274473cfd26cea15735fbb5585d550e1dc469
Sebastien Deraspe videotron <sebastien.deraspe@videotron.com>
2021-03-01 10:33:38 -0500

    CSS for the Card Butto, add Selection Card - (not finish)

---
+++ projects/front-end-library/src/lib/components/cart-selection/angular/cartSelection.module.ts
@@ -0,0 +1,9 @@
+import { NgModule } from '@angular/core';
+import { CartSelectionComponent } from './cartSelection.component';
+
+@NgModule({
+  declarations: [CartSelectionComponent],
+  imports: [],
+  exports: [CartSelectionComponent]
+})
+export class CardSelectionModule { }
