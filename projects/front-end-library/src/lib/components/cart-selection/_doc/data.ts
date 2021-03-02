2ab610cd5bca780a8da8360a23dff5f53f017df6
Sebastien Deraspe videotron <sebastien.deraspe@videotron.com>
2021-03-02 12:51:36 -0500

    add Cart Selection componant to the DS

--- projects/front-end-library/src/lib/components/cart-selection/_doc/data.ts
+++ projects/front-end-library/src/lib/components/cart-selection/_doc/data.ts
@@ -1,4 +1,15 @@
 export const cart = [
-
+    {
+        title          : 'Samsung Galaxy Note 20 Utra 5G',
+        description    : 'Navy Blue, 128 Go',
+        price         : '$24.00',
+        saving         : '$11',
+        fullPrice         : '$35',
+    },
+    {
+        title          : 'All-inclusive 12 GB Mobile',
+        description    : 'Lorem ipsum',
+        price         : '$55.00',
+    },
 ];
 
