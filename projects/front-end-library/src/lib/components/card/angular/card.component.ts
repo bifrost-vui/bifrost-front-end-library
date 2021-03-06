bc16facdc8bf0a7579d34cb5cf84e400c5871926
Baptiste <baptiste.mac@gmail.com>
2021-03-06 00:19:35 -0500

    Card: Add image and icon

--- projects/front-end-library/src/lib/components/card/angular/card.component.ts
+++ projects/front-end-library/src/lib/components/card/angular/card.component.ts
@@ -13,20 +13,26 @@ import { Component, ViewEncapsulation, OnInit, Input, Output, EventEmitter} from
 export class CardComponent implements OnInit {
     constructor() {}
 
+    @Input() image                  : object;
+    @Input() iconName               : string;
+
     @Input() upperTitle             : string;
     @Input() title                  : string;
     @Input() description            : string;
     @Input() link                   : object;
     @Input() badge                  : string;
+
     @Input() contentLists           : object;
+
     @Input() price                  : object;
+    @Input() note                   : string;
     @Input() buttons                : object;
     @Input() message                : string;
+    
     @Input() class                  : string;
     @Input() disabled               : boolean;
 
     ngOnInit() {
         console.log('card', this);
     }
-    
 }
