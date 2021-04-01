097ba9d1a1eba3a872c6a5de2b3a60c71f168adb
Baptiste <baptiste.mac@gmail.com>
2021-03-31 23:33:18 -0400

    carousel: document use-cases

---
+++ projects/front-end-library/src/lib/components/carousel/angular/carousel.component.ts
@@ -0,0 +1,29 @@
+import { Component, ViewEncapsulation, OnInit, Input } from '@angular/core';
+
+/**
+ * Description of the component Carousel
+ *
+ */
+ 
+@Component({
+  selector: 'bf-carousel',
+  templateUrl: './carousel.component.html',
+  // styleUrls: ['../scss/index.scss'],
+})
+export class CarouselComponent implements OnInit {
+    constructor() {}
+
+    /**
+    * The inner text.
+    *
+    * @required
+    */
+    @Input() label: string;
+
+    /** Class override */
+    @Input() class: string;
+
+    ngOnInit() {
+        console.log('Carousel', this);
+    }
+}
