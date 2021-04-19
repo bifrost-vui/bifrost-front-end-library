b5d8cb26e14d72953b10b69a2ba08f211a3bc685
Baptiste <baptiste.mac@gmail.com>
2021-04-19 12:46:59 -0400

    list: init

---
+++ projects/front-end-library/src/lib/components/list/angular/list.component.ts
@@ -0,0 +1,34 @@
+import { Component, ViewEncapsulation, OnInit, Input } from '@angular/core';
+
+/**
+ * Description of the component List
+ *
+ */
+ 
+@Component({
+  selector: 'bf-list',
+  templateUrl: './list.component.html',
+  // styleUrls: ['../scss/index.scss'],
+})
+export class ListComponent implements OnInit {
+    constructor() {}
+
+    @Input() title              : string;
+    @Input() titleTag           : string;
+    /** Expected format: 
+    ```
+    [{
+        label   : "Label",
+        iconName: "placeholder"
+    }]
+    ```
+    */
+    @Input() content            : object;
+
+    @Input() reversed           : boolean;
+    @Input() class              : string;
+
+    ngOnInit() {
+        console.log('List', this);
+    }
+}
