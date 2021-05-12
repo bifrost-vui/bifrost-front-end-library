75292110735d7dc6045030ef5f86e7785ebc6d1e
VIDEOTRON\lefebjon <jonathan.lefebvreinf@videotron.com>
2021-05-12 16:26:35 -0400

    DS-60: Fix Block selection item actions

--- projects/front-end-library/src/lib/components/block-selection/_doc/data.ts
+++ projects/front-end-library/src/lib/components/block-selection/_doc/data.ts
@@ -13,11 +13,20 @@ export const contentListsData = [
         priceInfo       : '/ month',
         saving          : '$30.00',
         fullPrice       : '$150.00',
+        btnDelete       : {
+            href        : 'www.videotron.com',
+        },
+        btnModify       : {
+            href        : 'www.videotron.com',
+        },
     },
     {
         title           : 'All-inclusive 12 GB Mobile',
         description     : 'Lorem ipsum',
         price           : '$55.00',
+        btnModify       : {
+            href        : 'www.videotron.com',
+        },
     },
 ];
 
