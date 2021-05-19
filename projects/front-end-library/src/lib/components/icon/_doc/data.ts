00a0c99fc65e88ae261ecdcad16e14e835649f1f
Nahalie Toussaint <Nathalie.Toussaint@videotron.com>
2021-05-19 16:08:39 -0400

    WIP : ajout des nouveaux svg et reconstitution de la liste

--- projects/front-end-library/src/lib/components/icon/_doc/data.ts
+++ projects/front-end-library/src/lib/components/icon/_doc/data.ts
@@ -1,375 +1,284 @@
-
+export const iconSizeSuffix=[
+  {
+    'xsmall': 'sm',
+    'small': 'sm',
+    'medium': 'lg',
+    'large': 'lg'
+  }
+];
 
 export const interfaceIcons = [
-    {
-        category: 'Common controls',
-        list: [
-            'cancel',
-            'out',
-            'hourglass',
-            'edit',
-            'bin',
-            'lock',
-            'menu-burger',
-            'menu-option',
-            'repeat',
-            'search',
-            'settings',
-            'eye-dash',
-            'eye',
-        ]
-    },
-    {
-        category: 'Arrows and chevrons',
-        list: [
-            'chevron-down',
-            'chevron-left',
-            'chevron-right',
-            'chevron-up',
-            'dot',
-            'interpunct',
-            'order-asc',
-            'order-desc',
-            'order-none',
-        ]
-    },
-    {
-        category: 'Add and remove',
-        list: [
-            'x',
-            'checkmark',
-            'plus',
-            'minus',
-            'checkmark-circle',
-            'x-circle-filled',
-            'plus-circle-filled',
-            'minus-circle-filled',
-            'checkmark-circle-filled',
-        ]
-    },
-    {
-        category: 'Attention',
-        list: [
-            'question-circle',
-            'exclamation-circle',
-            'information-circle',
-            'exclamation-circle-filled',
-            'information-circle-filled',
-            'question-circle-small',
-            'exclamation-circle-filled-small24',
-            'information-circle-filled-small24',
-        ]
-    },
-    {
-        category: 'Links and medias',
-        list: [
-            'link',
-            'external-link',
-            'camera',
-            'file-pdf',
-            'tutorial',
-        ]
-    },
-    {
-        category: 'Rating',
-        list: [
-            'star-empty',
-            'star-full',
-            'star-half',
-        ]
-    },
-    {
-        category: 'Communication',
-        list: [
-            'email',
-            'call',
-            'call-incoming',
-            'voice-command',
-            'chat',
-            'mobile-livechat',
-            'actualite',
-            'communaute',
-            'notification',
-            'building',
-            'printer',
-        ]
-    },
-    {
-        category: 'Time and place',
-        list: [
-            'clock',
-            'calendar',
-            'pin',
-        ]
-    },
-    {
-        category: 'Users',
-        list: [
-            'profile',
-            'user-family',
-            'user',
-        ]
-    },
-    {
-        category: 'Download and upload',
-        list: [
-            'download',
-            'upload',
-            'circle-download',
-            'circle-upload',
-        ]
-    },
-    {
-        category: 'Social medias',
-        list: [
-            'facebook',
-            'instagram',
-            'youtube',
-            'twitter',
-        ]
-    },
-    {
-        category: 'Cast and devices',
-        list: [
-            'appli-helix',
-            'helix-tv',
-            'relais-wifi',
-            'mobile',
-            'television',
-        ]
-    },
-    {
-        category: 'Account',
-        list: [
-            'account-balance',
-            'installation',
-            'support',
-            'cart',
-            'hierarchy',
-        ]
-    },
-    {
-        category: 'TV Plans',
-        list: [
-            'forfait-tv-fan-sports',
-            'forfait-tv-la-decouverte',
-            'forfait-tv-la-totale',
-            'forfait-tv-le-fan-de-films-et-series',
-            'forfait-tv-le-plein-la-vue',
-            'forfait-tv-streaming',
-        ]
-    },
-    {
-        category: 'Services et options',
-        list: [
-            'helix-app',
-            'tv-4k',
-            'tv-forward',
-            'tv-record',
-            'casting',
-            'cloud-dvr',
-            'unlimited',
-            'parental-control',
-            'wifi',
-            'world',
-            'play-circle',
-        ]
-    },
-    {
-        category: 'Internet plans - options',
-        list: [
-            'helixinternet-jeuxvideos',
-            'helixinternet-maison-connecte',
-            'helixinternet-musique',
-            'helixinternet-streaming',
-            'helixinternet-wifi_ultime',
-        ]
-    },
-    {
-        category: 'Documentation',
-        list: [
-            'placeholder',
-        ]
-    }
-];
+  {
+    category: 'Add and remove',
+    list: [
+      'check-filled',
+      'check',
+      'minus-circle-filled',
+      'minus-circle-outline',
+      'minus',
+      'plus-circle-filled',
+      'plus-circle-outline',
+      'plus',
+      'x-circle-filled',
+      'x-circle-outline',
+      'x'
+    ]
+  },
+  {
+    category: 'Attention',
+    list: [
+      'information-circle-filled',
+      'information',
+      'question',
+      'exclamation-circle-filled'
+    ]
+  },
+  {
+    category: 'Arrows and chevrons',
+    list: []
+  },
+  {
+    category: 'Common controls',
+    list: [
+      'delete',
+      'cancel',
+      'edit',
+      'eye-dash',
+      'eye',
+      'hourglass',
+      'menu-burger',
+      'menu-option',
+      'notification',
+      'logout',
+      'printer',
+      'repeat',
+      'settings'
+    ]
+  },
+  {
+    category: 'Links and medias',
+    list: [
+      'download',
+      'external-link',
+      'link',
+      'upload'
+    ]
+  },
+  {
+    category: 'Rating',
+    list: [
+      'star-empty',
+      'star-full',
+      'star-half'
+    ]
+  },
+  {
+    category: 'Social medias',
+    list: [
+      'facebook',
+      'instagram',
+      'twitter',
+      'youtube'
+    ]
+  }
+]
 
 export const illustrationIcons = [
-    {
-        category: 'Main',
-        list: [
-            'accessibility',
-            'actuality',
-            'agility',
-            'apple',
-            'application-client-area',
-            'application-helix-tv',
-            'applications',
-            'availability-of-service',
-            'bill',
-            'business',
-            'calendar',
-            'call-me-back',
-            'call-me-now',
-            'canada',
-            'cart',
-            'check',
-            'checklist',
-            'chrono',
-            'community',
-            'contact-phone',
-            'customer-service',
-            'data',
-            'delivery',
-            'desinstallation',
-            'differenciation',
-            'email',
-            'helix-mic',
-            'hosting-data',
-            'hosting',
-            'information',
-            'innovation',
-            'installation',
-            'instant-message',
-            'instructions',
-            'international',
-            'invoice-deadline-48h',
-            'moving-in',
-            'my-account',
-            'network-coverage-1',
-            'network-coverage',
-            'network-performance',
-            'new-client',
-            'notifications',
-            'pause',
-            'performance',
-            'play',
-            'point-of-sale',
-            'pre-authorized-payment',
-            'quebec',
-            'question',
-            'recycling',
-            'replay',
-            'search',
-            'second-hand',
-            'settings',
-            'simultaneous-recordings',
-            'smart-home',
-            'sound-off',
-            'sound-on',
-            'store',
-            'teamwork',
-            'time',
-            'travel',
-            'trio-wifi',
-            'troubleshooting',
-            'united-states',
-            'update',
-            'user-1',
-            'user-2',
-            'user-3',
-            'user-4',
-            'verify-eligibility',
-            'videotron-plus',
-            'wifi',
-        ]
-    },
-        {
-        category: 'Options',
-        list: [
-            'casting',
-            'cloud-dvr',
-            'connection-reliable',
-            'download-speed',
-            'memory',
-            'security',
-            'student-discount',
-            'subtitles',
-            'unlimited',
-            'upload-speed',
-            'video-quality-4K',
-            'video-quality-720p',
-            'video-quality-1080p',
-            'warranty-30-days',
-        ]
-    },
-    {
-        category: 'Options Mobility',
-        list: [
-            'water-resistance',
-            'battery',
-            'screen-size',
-            'camera',
-            'option-all-inclusive',
-            'option-voicemail-email',
-            'option-voicemail',
-            'mobile-bundle-packages',
-            'mobile-call-forwarding',
-            'mobile-call-planning',
-            'mobile-call-waiting',
-            'mobile-conf-call',
-            'mobile-display',
-            'mobile-flex-cell',
-            'mobile-international-calls',
-            'mobile-large-format',
-            'mobile-money-saving',
-            'mobile-multiline',
-            'mobile-protection',
-            'mobile-unlimited',
-            'mobile-voicemail-visual',
-        ]
-    },
-    {
-        category: 'Navigation',
-        list: [
-            'arrow-top',
-            'arrow-bottom',
-            'arrow-left',
-            'arrow-right',
-            'checkmark',
-            'arrow-close',
-            'size-reduce',
-            'size-enlarge',
-            'arrow-plus',
-        ]
-    },
-    {
-        category: 'Devices',
-        list: [
-            'bundle-1',
-            'bundle-2',
-            'bundle-3',
-            'gateway-helix-1',
-            'gateway-helix-2',
-            'pod-relais-view-1',
-            'pod-relais-view-2',
-            'pod-relais-view-3',
-            'remote',
-            'terminal-remote',
-            'terminal-top',
-            'terminal',
-            'card-memory',
-            'card-sim',
-            'television',
-            'helix-tv-remote',
-            'helix-tv',
-            'internet-residential',
-            'laptop-mobile',
-            'multi-product',
-            'mobility',
-            'telephony-residential',
-            'tablet-mobile-helix',
-            'other-services',
-        ]
-    },
-    {
-        category: 'Forfait',
-        list: [
-            'forfait-le -plein-la-vue',
-            'forfait-le-fan-de-sports',
-            'forfait-la-decouverte',
-            'forfait-la-totale',
-            'forfait-le-fan-de-films-et-series',
-            'forfait-le-leger',
-        ]
-    }
-];
+  {
+    category: 'abstract'
+    , list: [
+      'accessibility',
+      'agility',
+      'handshake',
+      'innovation',
+      'performance'
+    ]
+  }, {
+    category: 'application',
+    list: [
+      'application-client-area',
+      'application-helix-tv',
+      'application-stream',
+      'applications',
+      'videotron-plus'
+    ]
+  }, {
+    category: 'communication',
+    list: [
+      'call-me-back',
+      'call-me-now',
+      'community',
+      'contact-phone',
+      'customer-service',
+      'email',
+      'instant-message',
+      'notifications',
+      'pin',
+      'store',
+      'teamwork'
+    ]
+  }, {
+    category: 'features',
+    list: [
+      'battery',
+      'camera',
+      'memory',
+      'screen-size',
+      'water-resistance'
+    ]
+  }, {
+    category: 'feedback',
+    list: [
+      'check-outline',
+      'information-circle-filled',
+      'information',
+      'question'
+    ]
+  }, {
+    category: 'hardware',
+    list: [
+      'bundle-1',
+      'bundle-2',
+      'bundle-3',
+      'card-memory',
+      'card-sim',
+      'gateway-helix',
+      'helix-tv',
+      'helix-tv-remote',
+      'internet-residential',
+      'laptop-mobile',
+      'mobility',
+      'multi-product',
+      'other-services',
+      'pod-relais-view-1',
+      'pod-relais-view-2',
+      'pod-relais-view-3',
+      'remote',
+      'tablet-mobile-helix',
+      'telephony-residential',
+      'television',
+      'terminal',
+      'terminal-remote',
+      'terminal-top'
+    ]
+  }, {
+    category: 'media & data',
+    list: [
+      'actuality',
+      'data-graph',
+      'download-speed',
+      'helix-mic',
+      'hosting-data',
+      'hosting',
+      'pause',
+      'play',
+      'replay',
+      'sound-off',
+      'sound-on',
+      'subtitles',
+      'unlimited',
+      'update',
+      'upload-speed',
+      'video-quality-1080p',
+      'video-quality-4k',
+      'video-quality-720p'
+    ]
+  }, {
+    category: 'network',
+    list: [
+      'network-coverage-canada',
+      'network-coverage',
+      'network-performance',
+      'trio-wifi',
+      'wifi'
+    ]
+  }, {
+    category: 'options & add-ons',
+    list: [
+      'apple',
+      'mobile-bundle-packages',
+      'mobile-call-forwarding',
+      'mobile-call-planning',
+      'mobile-call-waiting',
+      'mobile-conf-call',
+      'mobile-display',
+      'mobile-flex-cell',
+      'mobile-international-calls',
+      'mobile-large-format',
+      'mobile-money-saving',
+      'mobile-multiline',
+      'mobile-protection',
+      'mobile-unlimited',
+      'mobile-voicemail-visual',
+      'option-all-inclusive',
+      'option-voicemail-email',
+      'option-voicemail'
+    ]
+  }, {
+    category: 'payment',
+    list: [
+      'hierarchy'
+    ]
+  }, {
+    category: 'services',
+    list: [
+      'availability-of-service',
+      'business',
+      'canada',
+      'casting',
+      'cloud-dvr',
+      'connection-reliable',
+      'international',
+      'quebec',
+      'search',
+      'security',
+      'smart-home',
+      'travel',
+      'united-states',
+      'verify-eligibility'
+    ]
+  }, {
+    category: 'support',
+    list: [
+      'business-large-sized',
+      'business-small-sized',
+      'checklist',
+      'delivery',
+      'desinstallation',
+      'installation',
+      'instructions',
+      'moving-in',
+      'recycling',
+      'second-hand',
+      'settings-1',
+      'troubleshooting'
+    ]
+  }, {
+    category: 'time',
+    list: [
+      'calendar-check',
+      'calendar',
+      'chrono',
+      'clock'
+    ]
+  }, {
+    category: 'tv plans',
+    list: [
+      'tv-plan-absolute',
+      'tv-plan-all-in',
+      'tv-plan-discovery',
+      'tv-plan-light',
+      'tv-plan-showtime',
+      'tv-plan-sports'
+    ]
+  }, {
+    category: 'users',
+    list: [
+      'my-account',
+      'new-client',
+      'user-1',
+      'user-2',
+      'user-3',
+      'user-4'
+    ]
+  }];
