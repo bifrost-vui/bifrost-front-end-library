8184f013fd74ae067c6d50383ea5c0eabf80508d
Nahalie Toussaint <Nathalie.Toussaint@videotron.com>
2021-06-09 11:04:46 -0400

    components/icones-tabTMO-DS-45 - post review

--- projects/front-end-library/src/lib/components/icon/angular/icon.component.ts
+++ projects/front-end-library/src/lib/components/icon/angular/icon.component.ts
@@ -13,7 +13,7 @@ import { Component, ViewEncapsulation, OnInit, Input, Output, EventEmitter} from
 export class IconComponent implements OnInit {
     constructor() {}
 
-    @Input() type               : string;
+    @Input() type               : 'interface' | 'illustration';
     @Input() name               : string;
     @Input() size               : 'xsmall' | 'small' | 'medium' | 'large';
     @Input() color              : ''| 'success' | 'neutral'| 'warning' | 'negative';
