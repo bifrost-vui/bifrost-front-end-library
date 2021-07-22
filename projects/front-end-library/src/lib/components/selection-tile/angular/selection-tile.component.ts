fa1af3f31dbfb2c6c57933ba070861cc3e9fa023
Nathalie Toussain <nathalie.toussaint@videotron.com>
2021-07-22 14:24:33 -0400

    DS-66-selection-tile - takeover

--- projects/front-end-library/src/lib/components/selection-tile/angular/selection-tile.component.ts
+++ projects/front-end-library/src/lib/components/selection-tile/angular/selection-tile.component.ts
@@ -13,15 +13,25 @@ import { Component, ViewEncapsulation, OnInit, Input } from '@angular/core';
 export class SelectionTileComponent implements OnInit {
     constructor() {}
 
-    /**
-    * The inner text.
-    *
-    * @required
-    */
-    @Input() label: string;
-
     /** Class override */
     @Input() class: string;
+    /** required */
+     @Input() inputType   : 'radio'|'checkbox';
+    /** required */
+    @Input() inputId      : string;
+    /** required */
+      @Input() value      : string;
+    /** required */
+    @Input() ariaLabel    : string;
+    @Input() name         : string;
+
+     @Input() orientation : 'vertical'|'horizontal';
+     @Input() size        : 'small'|'large';
+     @Input() reversed    : boolean;
+     @Input() isDisabled  : boolean; 
+     @Input() isSelected  : boolean; 
+
+
 
     ngOnInit() {
         console.log('SelectionTile', this);
