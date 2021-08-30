14c5f0fdd8e9cb49edc6f30a9efa5f40db9daf9d
Baptiste <baptiste.mac@gmail.com>
2021-08-30 17:53:21 -0400

    add icon-library angular project

---
+++ projects/icon-library/src/lib.module.ts
@@ -0,0 +1,10 @@
+import { NgModule } from '@angular/core';
+import { IconModule } from './lib/components/icon/angular/icon.module';
+
+@NgModule({
+  declarations: [IconModule],
+  imports: [
+  ],
+  exports: [IconModule],
+})
+export class LibModule { }
