6fdcdc7f61e647f9046242b1f5a342d58e8378d7
Thalie Toussaint <Nathalie.Toussaint@videotron.com>
2021-08-31 09:06:59 -0400

    DS-66-selection-tile - fixed `size` namming issues (now fix figma for coherence) - updated css behaviors effect - udated storybook layout + documentation

--- projects/front-end-library/src/lib/components/selection-tile/angular/selection-tile.component.ts
+++ projects/front-end-library/src/lib/components/selection-tile/angular/selection-tile.component.ts
@@ -21,7 +21,7 @@ export class SelectionTileComponent implements OnInit {
     @Input() extraAttribute : string
 
     @Input() orientation    : 'horizontal'|'vertical';
-    @Input() size           : 'fit-content'|'fit-container';
+    @Input() fit            : 'content'|'container';
     @Input() title          : string;
     @Input() description    : string;
     @Input() info           : string;
