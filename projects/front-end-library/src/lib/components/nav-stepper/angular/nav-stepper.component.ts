f8ed38e64b062555cddb6f0756727dc266d01548
Thalie Toussaint <Nathalie.Toussaint@videotron.com>
2021-09-15 10:50:44 -0400

    DS-139-navigation-stepper Updates suite à la découverte d'un autre figma plus détaillé

--- projects/front-end-library/src/lib/components/nav-stepper/angular/nav-stepper.component.ts
+++ projects/front-end-library/src/lib/components/nav-stepper/angular/nav-stepper.component.ts
@@ -6,22 +6,26 @@ import { Component, ViewEncapsulation, OnInit, Input } from '@angular/core';
  */
 
 @Component({
-  selector: 'bf-nav-stepper',
+  selector: 'bf-stepper',
   templateUrl: './nav-stepper.component.html',
   // styleUrls: ['../scss/index.scss'],
 })
 export class NavStepperComponent implements OnInit {
     constructor() {}
 
-    /**
-    * The inner text.
-    *
-    * @required
-    */
-    @Input() navItemSteps : object;
-    @Input() activeStep   : number;
-    @Input() class        : string;
-    @Input() reversed     : boolean;
+    /** Expected :
+     *  [{label:"stepName",href:"/path-to-file"}, {...}]
+     */
+    @Input() navItemSteps     : object;
+    @Input() activeStep       : number;
+    @Input() class            : string;
+    /** hasStepCounter must be set to true in order for the label to display*/
+    @Input() counterLabel     : string;
+    @Input() previousHasLabel : boolean;
+    @Input() nextHasLabel     : boolean;
+    @Input() nextIsClickable  : boolean;
+    @Input() hasStepCounter   : boolean;
+    @Input() reversed         : boolean;
 
     ngOnInit() {
         console.log('NavStepper', this);
