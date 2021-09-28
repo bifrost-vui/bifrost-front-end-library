f793948711daddd4e706b625721e56bcd3ef0c4b
seckfatm <FatouMareme.Seck@videotron.com>
2021-09-28 15:06:51 -0400

    Modified options for displaying simple or double button

--- projects/front-end-library/src/lib/components/hero/angular/hero.component.ts
+++ projects/front-end-library/src/lib/components/hero/angular/hero.component.ts
@@ -20,8 +20,10 @@ export class HeroComponent implements OnInit {
     @Input() title              : string;
     @Input() subtitle           : string;
     @Input() description        : string;
+    @Input() buttonType         : 'button' | 'doubleButton';
+    @Input() button             : object;
   /**
-   Insert button with a limited array of objects of 1 or 2 items.
+   Insert buttons (max of 2 items). Explore its [Component API](/?path=/docs/components-button--drupal).
    */
     @Input() buttons            : Array<object>[];
     @Input() image              : object;
