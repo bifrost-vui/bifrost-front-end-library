dcdc9e02cebdbab4522a0bf65307544febdf2b90
Thalie Toussaint <Nathalie.Toussaint@videotron.com>
2021-10-01 16:28:52 -0400

    DS-166-slideshow - fixs après avoir trouvé d'autres CR de Baptiste

--- projects/front-end-library/src/lib/components/slideshow/_doc/data.ts
+++ projects/front-end-library/src/lib/components/slideshow/_doc/data.ts
@@ -1,11 +1,14 @@
 export const slides=[
     {
         class       : "",
-        badge       : {label: "badge label with icon", iconName: "camera"},
+        badge       : {
+            label       : "badge label with icon",
+            iconName    : "camera"
+        },
         upperTitle  : "Upper Title",
         title       : "Slide Title",
-        subtitle    : "Upper Title",
-        description : "Lorem ipsum descriptum est",
+        subtitle    : "Subtitle",
+        description : "Description description description description description description description description description",
         button      : {
             label       : "Button",
             hierarchy   : "primary-alt",
@@ -14,51 +17,33 @@ export const slides=[
         background      : "highlight",
     },
     {
-        upperTitle  : "Lorem ipsum dolor sit amet",
+        upperTitle  : "Upper Title",
         title       : "Slide Title",
-        subtitle    : "Upper Title",
-        description : "Lorem ipsum descriptum est",
+        subtitle    : "subtitle",
+        description : "Description text",
         button      : {
             label       : "Button",
             hierarchy   : "primary-alt",
             class       : "my-class"
         },
-        image           : {
-            default : "/images/_docs/hero-helix.png",
-            mobile  : "/images/_docs/hero-helix-mobile.png",
-            alt     : "Helix"
+        image       : {
+            default     : "/images/_docs/hero-helix.png",
+            mobile      : "/images/_docs/hero-helix-mobile.png",
+            alt         : "Helix"
         },
-        background      : "ground",
+        background  : "ground",
     },
     {
         class       : "reversed",
-        badge       : "Lorem ipsum",
+        badge       : "Badge label",
         upperTitle  : "Upper Title",
-        title       : "Slide Title",
-        subtitle    : "Lorem ipsum dolor sit amet.",
+        title       : "Slide title slide title slide title slide title slide title ",
+        subtitle    : "Suptitle suptitle suptitle suptitle suptitle suptitle",
         description : "Lorem ipsum descriptum est",
         button      : {
             label       : "Button",
             hierarchy   : "primary-alt",
             class       : ""
-        },
-        blockSelection  : {
-            title               : 'My selection',
-            month               : 'month',
-            contentLists        : [
-                {
-                    title           : 'Samsung Galaxy Note 20 Utra 5G',
-                    description     : 'Navy Blue, 128 Go',
-                    price           : '$24.00',
-                    saving          : '$30.00',
-                    fullPrice       : '$150.00',
-                },
-                {
-                    title           : 'All-inclusive 12 GB Mobile',
-                    description     : 'Lorem ipsum',
-                    price           : '$55.00',
-                },
-            ],
         },
         background      : "",
     },
@@ -67,17 +52,16 @@ export const slides=[
         badge       : 'badge label',
         upperTitle  : "Upper Title",
         title       : "Slide Title",
-        subtitle    : "Lorem ipsum dolor sit amet.",
-        description : "Lorem ipsum descriptum est",
+        subtitle    : "Subtitle subtitle subtitle subtitle ",
         button      : {
             label       : "Button",
             hierarchy   : "primary-alt",
             class       : ""
         },
-        image           : {
-            default : "/images/_docs/hero-helix-reversed.png",
-            mobile  : "/images/_docs/hero-helix-reversed-mobile.png",
-            alt     : "Helix"
+        image       : {
+            default     : "/images/_docs/hero-helix-reversed.png",
+            mobile      : "/images/_docs/hero-helix-reversed-mobile.png",
+            alt         : "Helix"
         },
         background      : "reversed",
     },
