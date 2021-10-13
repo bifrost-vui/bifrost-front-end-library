7b2776043bb767522dded0d17b77141a3955fb0d
Thalie Toussaint <Nathalie.Toussaint@videotron.com>
2021-10-13 14:04:31 -0400

    DS-66-selection-tile - added option for `required` html attribute

--- projects/front-end-library/src/lib/components/selection-tile/angular/selection-tile.component.ts
+++ projects/front-end-library/src/lib/components/selection-tile/angular/selection-tile.component.ts
@@ -34,6 +34,7 @@ export class SelectionTileComponent implements OnInit {
     @Input() isSelected     : boolean;
     @Input() isInvalid      : boolean;
     @Input() errorMessage   : string;
+    @Input() required       : boolean;
 
     ngOnInit() {
         console.log('SelectionTile', this);
