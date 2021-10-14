561dc87e0bd8d47e723b3d06cb82d87cf01a6617
Baptiste <baptiste.mac@gmail.com>
2021-10-14 16:27:57 -0400

    upgrade storybook

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,19 +1,10 @@
-
-const path      = require('path');
-// const custom    = require('../webpack.config.js');
-
 module.exports = {
-    stories: ['../projects/front-end-library/**/*.stories.@(ts|mdx)'],
+    stories: ['../projects/front-end-library/src/lib/components/badge/**/*.stories.@(ts|mdx)'],
     logLevel: 'debug',
     addons: [
-        '@storybook/addon-docs',
-        '@storybook/addon-controls',
+        '@storybook/addon-essentials',
         '@storybook/addon-a11y',
-        '@storybook/addon-backgrounds',
         '@storybook/addon-links',
         'storybook-addon-designs',
     ],
-    // webpackFinal: (config) => {
-    //     return { ...config, module: { ...config.module, rules: custom.module.rules } };
-    // },
 };
