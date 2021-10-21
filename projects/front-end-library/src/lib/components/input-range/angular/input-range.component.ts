0fe0bf65ba3704e685a08074c3d490f972a08b6d
Thalie Toussaint <Nathalie.Toussaint@videotron.com>
2021-10-21 15:59:33 -0400

    DS-165-range-slider - post mini Baptist review

--- projects/front-end-library/src/lib/components/input-range/angular/input-range.component.ts
+++ projects/front-end-library/src/lib/components/input-range/angular/input-range.component.ts
@@ -11,9 +11,10 @@ export class InputRangeComponent implements OnInit {
     @Input() maxInputId     : string;
     @Input() minInputValue  : number;
     @Input() maxInputValue  : number;
-    /** if no `minStartValue` is defined, `minInputValue` will be used instead  */
+
+    /** if no `minStartValue` is defined, `minInputValue` will be the min start value */
     @Input() minStartValue  : number;
-    /** if no `maxStartValue` is defined, `maxInputValue` will be used instead  */
+    /** if no `maxStartValue` is defined, `maxInputValue` will be the max start value */
     @Input() maxStartValue  : number;
     @Input() labelUnit      : string;
     @Input() step           : number;
@@ -22,8 +23,8 @@ export class InputRangeComponent implements OnInit {
     @Input() rangeHandles   :''|'min' |'max';
     @Input() minLegend      : string;
     @Input() maxLegend      : string;
-    @Input() minLegendDesc  : string;
-    @Input() maxLegendDesc  : string;
+    @Input() minLegendDescription  : string;
+    @Input() maxLegendDescription  : string;
     @Input() class          : string;
 
     ngOnInit() {
