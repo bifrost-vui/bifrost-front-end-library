834ee543e877bc9f019d486cc8e7b0ecb285b3fe
Thalie Toussaint <Nathalie.Toussaint@videotron.com>
2021-10-27 12:28:56 -0400

    DS-165-range-slider - correction nommenclature BEM (double '_')

--- projects/front-end-library/src/lib/components/input-range/js/index.js
+++ projects/front-end-library/src/lib/components/input-range/js/index.js
@@ -6,21 +6,21 @@ $(function() {
     container.each(function (i, el) {
         const $this = $(el),
         //inputs
-        $minRangeInput = $this.find('.bf-input-range_input_min'),
-        $maxRangeInput = $this.find('.bf-input-range_input_max'),
+        $minRangeInput = $this.find('.bf-input-range__input__min'),
+        $maxRangeInput = $this.find('.bf-input-range__input__max'),
         //inputs attributes
         $minInputValue = $minRangeInput.attr('min'),
         $maxInputValue = $minRangeInput.attr('max'),
         $unit = $minRangeInput.attr('data-unit'),
         //overlay divs
-        $excludeLeft = $this.find('.bf-input-range_exclude--left'),
-        $excludeRight = $this.find('.bf-input-range_exclude--right'),
-        $rangeValue = $this.find('.bf-input-range_value'),
-        $handleLeft = $this.find('.bf-input-range_handle--left'),
-        $handleRight = $this.find('.bf-input-range_handle--right'),
+        $excludeLeft = $this.find('.bf-input-range__exclude--left'),
+        $excludeRight = $this.find('.bf-input-range__exclude--right'),
+        $rangeValue = $this.find('.bf-input-range__value'),
+        $handleLeft = $this.find('.bf-input-range__handle--left'),
+        $handleRight = $this.find('.bf-input-range__handle--right'),
         //labels
-        $minLabel = $this.find('.bf-input-range_handle--left .bf-input-range_label'),
-        $maxLabel = $this.find('.bf-input-range_handle--right .bf-input-range_label');
+        $minLabel = $this.find('.bf-input-range__handle--left .bf-input-range__label'),
+        $maxLabel = $this.find('.bf-input-range__handle--right .bf-input-range__label');
 
         let suffix = '';
         let prefix = '';
@@ -58,11 +58,11 @@ $(function() {
             return (100/(parseInt($maxInputValue) - parseInt($minInputValue))) * parseInt(rangeInput) - (100/(parseInt($maxInputValue) - parseInt($minInputValue))) * parseInt($minInputValue)
         }
 
-        $this.on('input', '.bf-input-range_input_min', function() {
+        $this.on('input', '.bf-input-range__input__min', function() {
             displayCurrentValue();
         });
 
-        $this.on('input', '.bf-input-range_input_max', function(){
+        $this.on('input', '.bf-input-range__input__max', function(){
             displayCurrentValue();
         })
     });
