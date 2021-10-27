4824dde1c542900c99cf5d986eea3433622c54be
Thalie Toussaint <Nathalie.Toussaint@videotron.com>
2021-10-27 15:30:51 -0400

    DS-165-range-slider - refactoring api : value + variants

--- projects/front-end-library/src/lib/components/input-range/angular/input-range.component.ts
+++ projects/front-end-library/src/lib/components/input-range/angular/input-range.component.ts
@@ -7,6 +7,8 @@ import { Component, ViewEncapsulation, OnInit, Input } from '@angular/core';
 })
 export class InputRangeComponent implements OnInit {
     constructor() {}
+
+    @Input() initialValue   : number | Array<number> ;
     /** inputId is required and if no `startValue` is defined, `InputValue` will be the min start*/
     @Input() min            : object;
     /** inputId is required and if no `startValue` is defined, `InputValue` will be the max start*/
@@ -16,8 +18,7 @@ export class InputRangeComponent implements OnInit {
     @Input() step           : number;
     /** determines the positionning `labelUnit` when `labelUnit` is a `$` */
     @Input() language       : 'en'|'fr';
-    /** By default this component displays 2 handles. By selecting `min` or `max` the component will display only one */
-    @Input() rangeHandles   : 'both'|'min' |'max';
+    @Input() isInverted     : boolean;
     @Input() class          : string;
 
     ngOnInit() {
