3355d5f659644f51d15074d3c48471537be76288
bibifrost <baptiste.mac+videotron@gmail.com>
2021-11-21 21:17:52 -0500

    add angular badge

--- projects/front-end-library/src/lib/components/badge/angular/badge.component.ts
+++ projects/front-end-library/src/lib/components/badge/angular/badge.component.ts
@@ -1,16 +1,15 @@
-import { Component, ViewEncapsulation, OnInit, Input, Output, EventEmitter} from "@angular/core";
+import { Component, ElementRef, OnInit, Input } from "@angular/core";
 
 
 @Component({
     selector: "bf-badge",
     templateUrl: './badge.component.html',
-    // styleUrls: ['../common/style.scss'],
 })
-export class BadgeComponent implements OnInit {
+export class BadgeComponent implements OnInit, OnChanges {
     constructor() {}
 
     @Input() label          : string;
-    @Input() hierarchy      : 'primary' | 'secondary' | 'tertiary' | 'tertiary-ground';
+    @Input() hierarchy      : 'primary' | 'secondary' | 'tertiary' | 'tertiary-ground' = 'secondary';
     @Input() noUppercase    : boolean;
     /** Displays an angle on a corner. Won't have any effect if `isRounded` is true*/
     @Input() angle          : '' | 'left' | 'right';
@@ -20,7 +19,23 @@ export class BadgeComponent implements OnInit {
     @Input() iconName       : string;
     @Input() reversed       : boolean;
 
+    public this.ngClasses = '';
+
+    computetNgClasses() {
+        this.ngClasses = [
+            String(this.class),
+            this.hierarchy                  ? 'bf-badge--' + this.hierarchy : 'bf-badge--secondary',
+            this.angle && !this.isRounded   ? 'bf-badge--angle-' + this.angle : '',
+            this.isRounded                  ? 'bf-badge--rounded' : ''
+        ]
+    }
+
     ngOnInit() {
         console.log('badge', this);
+        this.computetNgClasses();
+    }
+
+    ngOnChanges() {
+        this.computetNgClasses();
     }
 }
