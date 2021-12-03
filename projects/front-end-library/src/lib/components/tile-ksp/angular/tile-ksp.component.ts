4c7295eb135b9118351c19df06390c19c48b549b
bibifrost <baptiste.mac+videotron@gmail.com>
2021-12-03 18:04:26 -0500

    ds-228 init tile ksp

---
+++ projects/front-end-library/src/lib/components/tile-ksp/angular/tile-ksp.component.ts
@@ -0,0 +1,38 @@
+import { Component, ViewEncapsulation, OnInit, Input, Output, EventEmitter} from "@angular/core";
+
+interface IImage {
+    src         : string;
+}
+
+interface IIcon {
+    src         : string;
+    color       : null | 'success' | 'neutral' | 'warning' | 'negative';
+}
+
+@Component({
+    selector: 'bf-tile-ksp',
+    templateUrl: './tile-ksp.component.html',
+})
+export class TileKSPComponent implements OnInit {
+    constructor() {}
+
+    @Input() href               : string;
+    @Input() title              : string;
+    @Input() titleTag           : string;
+    @Input() description        : string;
+    @Input() linkLabel          : string;
+    /** Expected format: See [Icon API](?path=/docs/components-icon--drupal-interface). */
+    @Input() icon               : IIcon;
+    /** Expected format: `{ src: 'my-image.png' }` */
+    @Input() backgroundImage    : IImage;
+    @Input() backgroundColor    : 'ground' | 'underground' | 'highlight';
+    @Input() isClickable        : boolean;
+
+    @Input() class              : string;
+    @Input() reversed           : boolean;
+
+    ngOnInit() {
+        console.log('Tile KSP', this);
+    }
+
+}
---
+++ projects/front-end-library/src/lib/components/tile-ksp/angular/tile-ksp.module.ts
@@ -0,0 +1,9 @@
+import { NgModule } from '@angular/core';
+import { TileKSPComponent } from './tile-ksp.component';
+
+@NgModule({
+  declarations: [TileKSPComponent],
+  imports: [],
+  exports: [TileKSPComponent]
+})
+export class TileKSPModule { }
