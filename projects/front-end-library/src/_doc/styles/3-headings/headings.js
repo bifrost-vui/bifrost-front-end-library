8024393bbf2347daf779262083495e645cc58b9c
bibifrost <baptiste.mac+videotron@gmail.com>
2021-12-04 14:35:22 -0500

    ds-234 improve global styles

---
+++ projects/front-end-library/src/_doc/styles/3-headings/headings.js
@@ -0,0 +1,85 @@
+const headings = [
+    {
+        name            : 'h1',
+        themeVideotron  : {
+            size        : '1rem',
+            leading     : '1rem',
+            weight      : '1rem',
+            sizeSM      : '1rem',
+            leadingSM   : '1rem',
+            weightSM    : '1rem',
+        },
+        themeBusiness  : {
+            size        : '3.5rem',
+            leading     : '3.625rem',
+            weight      : '400',
+        }
+    },
+    {
+        name            : 'h2',
+        themeVideotron  : {
+            size        : '1rem',
+            leading     : '1rem',
+            weight      : '1rem',
+            sizeSM      : '1rem',
+            leadingSM   : '1rem',
+            weightSM    : '1rem',
+        },
+        themeBusiness  : {
+            size        : '2.5rem',
+            leading     : '2.75rem',
+            weight      : '400',
+        }
+    },
+    {
+        name            : 'h3',
+        themeVideotron  : {
+            size        : '1rem',
+            leading     : '1rem',
+            weight      : '1rem',
+            sizeSM      : '1rem',
+            leadingSM   : '1rem',
+            weightSM    : '1rem',
+        },
+        themeBusiness  : {
+            size        : '2rem',
+            leading     : '2.25rem',
+            weight      : '400',
+        }
+    },
+    {
+        name            : 'h4',
+        themeVideotron  : {
+            size        : '1rem',
+            leading     : '1rem',
+            weight      : '1rem',
+            sizeSM      : '1rem',
+            leadingSM   : '1rem',
+            weightSM    : '1rem',
+        }
+    },
+    {
+        name            : 'h5',
+        themeVideotron  : {
+            size        : '1rem',
+            leading     : '1rem',
+            weight      : '1rem',
+            sizeSM      : '1rem',
+            leadingSM   : '1rem',
+            weightSM    : '1rem',
+        }
+    },
+    {
+        name            : 'h6',
+        themeVideotron  : {
+            size        : '1rem',
+            leading     : '1rem',
+            weight      : '1rem',
+            sizeSM      : '1rem',
+            leadingSM   : '1rem',
+            weightSM    : '1rem',
+        }
+    },
+];
+
+export default headings;
