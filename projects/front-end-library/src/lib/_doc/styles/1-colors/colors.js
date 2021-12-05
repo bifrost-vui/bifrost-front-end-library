d9f3311dc3a354b31a1407ad3d4c1b5494446814
bibifrost <baptiste.mac+videotron@gmail.com>
2021-12-04 19:56:50 -0500

    ds-234 improve colors documentation

---
+++ projects/front-end-library/src/lib/_doc/styles/1-colors/colors.js
@@ -0,0 +1,128 @@
+const colors = [
+    {
+        name: 'Content',
+        list: [
+            {
+                label            : 'Neutral Primary',
+                name             : 'neutral-primary'
+            },
+            {
+                label            : 'Neutral Secondary',
+                name             : 'neutral-secondary'
+            },
+            {
+                label            : 'Neutral Tertiary',
+                name             : 'neutral-tertiary'
+            },
+        ]
+    },
+    {
+        name: 'Background',
+        list: [
+            {
+                label            : 'Ground',
+                name             : 'bg-ground'
+            },
+            {
+                label            : 'Underground',
+                name             : 'bg-underground'
+            },
+            {
+                label            : 'Highlight',
+                name             : 'bg-highlight'
+            },
+            {
+                label            : 'Brand',
+                name             : 'brand'
+            },
+            {
+                label            : 'Highlight',
+                name             : 'highlight',
+                hasUtilityClass  : false,
+            },
+            {
+                label            : 'bg-action-primary',
+                name             : 'bg-action-primary'
+            },
+            {
+                label            : 'bg-action-primary-hover',
+                name             : 'bg-action-primary-hover'
+            },
+            {
+                label            : 'bg-action-secondary',
+                name             : 'bg-action-secondary'
+            },
+            {
+                label            : 'bg-action-secondary-hover',
+                name             : 'bg-action-secondary-hover'
+            },
+            {
+                label            : 'bg-action-no-bg-hover',
+                name             : 'bg-action-no-bg-hover'
+            },
+            {
+                label            : 'bg-action-disabled',
+                name             : 'bg-action-disabled'
+            },
+            {
+                label            : 'bg-action-selected',
+                name             : 'bg-action-selected',
+                hasUtilityClass  : false,
+            },
+            {
+                label            : 'bg-overlay-black-20prct',
+                name             : 'bg-overlay-black-20prct',
+                hasUtilityClass  : false,
+            },
+            {
+                label            : 'bg-overlay-black-50prct',
+                name             : 'bg-overlay-black-50prct',
+                hasUtilityClass  : false,
+            },
+        ]
+    },
+    {
+        name: 'Feedback',
+        list: [
+            {
+                label            : 'Neutral',
+                name             : 'feedback-neutral',
+                hasUtilityClass  : false
+            },
+            {
+                label            : 'Success',
+                name             : 'feedback-success',
+                hasUtilityClass  : false
+            },
+            {
+                label            : 'Warning',
+                name             : 'feedback-warning',
+                hasUtilityClass  : false
+            },
+            {
+                label            : 'Negative',
+                name             : 'feedback-negative',
+                hasUtilityClass  : false
+            },
+        ]
+    },
+    {
+        name: 'Stroke',
+        list: [
+            {
+                label            : 'Primary',
+                name             : 'stroke-primary'
+            },
+            {
+                label            : 'Secondary',
+                name             : 'stroke-secondary'
+            },
+            {
+                label            : 'Tertiary',
+                name             : 'stroke-tertiary'
+            },
+        ]
+    },
+];
+
+export default colors;
