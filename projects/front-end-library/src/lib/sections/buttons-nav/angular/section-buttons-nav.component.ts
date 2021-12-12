a17bee746396c5043373f5f2bb353efd8acd66c3
bibifrost <baptiste.mac+videotron@gmail.com>
2021-12-11 22:56:03 -0500

    ds-254 init section buttons nav

---
+++ projects/front-end-library/src/lib/sections/buttons-nav/angular/section-buttons-nav.component.ts
@@ -0,0 +1,21 @@
+import { Component, ViewEncapsulation, OnInit, Input, Output, EventEmitter} from "@angular/core";
+
+@Component({
+    selector: "bf-section-buttons-nav",
+    templateUrl: './section-buttons-nav.component.html',
+})
+export class SectionButtonsNavComponent implements OnInit {
+    constructor() {}
+
+    @Input() title          : string;
+    @Input() titleTag       : string;
+    @Input() buttons        : object[];
+
+    @Input() class          : string;
+    @Input() reversed       : boolean;
+    @Input() highlight      : boolean;
+
+    ngOnInit() {
+        console.log('section buttons nav', this);
+    }
+}
---
+++ projects/front-end-library/src/lib/sections/buttons-nav/angular/section-buttons-nav.module.ts
@@ -0,0 +1,9 @@
+import { NgModule } from '@angular/core';
+import { SectionButtonsNavComponent } from './section-buttons-nav.component';
+
+@NgModule({
+  declarations: [SectionButtonsNavComponent],
+  imports: [],
+  exports: [SectionButtonsNavComponent]
+})
+export class SectionButtonsNavModule { }
