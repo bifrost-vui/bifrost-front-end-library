570203cec9b9b04783338b4885dc25455e4a5f9e
seckfatm <FatouMareme.Seck@videotron.com>
2021-12-17 16:32:49 -0500

    Create table of content Ds-225 Modify Tab component overflow-x scrolling right - -Ds-257 Integration Tab in Table of content - Ds-258 Integration Dropdown component in Table of content - -Ds-259

---
+++ projects/front-end-library/src/lib/components/table-content/angular/table-content.component.ts
@@ -0,0 +1,31 @@
+import { Component, ViewEncapsulation, OnInit, Input } from '@angular/core';
+
+/**
+ * Description of the component TableContent
+ *
+ */
+
+@Component({
+  selector: 'bf-table-content',
+  templateUrl: './table-content.component.html',
+  // styleUrls: ['../scss/index.scss'],
+})
+export class TableContentComponent implements OnInit {
+    constructor() {}
+
+    /**
+    * The inner text.
+    *
+    * @required
+    */
+
+    @Input() navItems     : Array<object>[];
+    @Input() label        : string;
+    @Input() reversed     : boolean;
+    @Input() class        : string;
+    @Input() isDisabled   : boolean;
+
+    ngOnInit() {
+        console.log('TableContent', this);
+    }
+}
---
+++ scripts/generate/templates/table-of-content/angular/table-of-content.module.ts
@@ -0,0 +1,10 @@
+import { NgModule } from '@angular/core';
+import { TableOfContentComponent } from './table-of-content.component';
+
+@NgModule({
+  declarations: [TableOfContentComponent],
+  imports: [
+  ],
+  exports: [TableOfContentComponent]
+})
+export class TableOfContentModule { }
