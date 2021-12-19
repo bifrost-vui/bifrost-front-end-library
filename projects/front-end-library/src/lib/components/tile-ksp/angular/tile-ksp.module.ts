6829d63277bdd6319f031648762c79df435855e3
bibifrost <baptiste.mac+videotron@gmail.com>
2021-12-19 02:54:20 +0100

    ds-233 rename tile-ksp to tile-key

--- projects/front-end-library/src/lib/components/tile-ksp/angular/tile-ksp.module.ts
+++
@@ -1,9 +0,0 @@
-import { NgModule } from '@angular/core';
-import { TileKSPComponent } from './tile-ksp.component';
-
-@NgModule({
-  declarations: [TileKSPComponent],
-  imports: [],
-  exports: [TileKSPComponent]
-})
-export class TileKSPModule { }
