b9017a65d0f0bb6bf5095bfea496a1d793be0a77
Nathalie Toussain <nathalie.toussaint@videotron.com>
2022-01-13 20:28:54 -0500

    DS-262 JS fix + adjusted demo data (for tests)

--- projects/front-end-library/src/lib/components/slideshow/js/index.js
+++ projects/front-end-library/src/lib/components/slideshow/js/index.js
@@ -1,6 +1,20 @@
 import $ from 'jquery';
 import '../../../js/components/slick-carousel.js'
 
+/*-------- Apply reversed on navigation controls -------------*/
+
+$('.bf-slideshow__inner').on('init', function(slick){
+    if ($('.slick-current .bf-hero').hasClass('reversed')){
+        $('.bf-slideshow .bfslick__controls').addClass('reversed')
+    }
+});
+$('.bf-slideshow__inner').on('afterChange', function(event, slick, currentSlide, nextSlide){
+    $('.bf-slideshow .bfslick__controls').removeClass('reversed')
+    if ($('.slick-current .bf-hero').hasClass('reversed')){
+        $('.bf-slideshow .bfslick__controls').addClass('reversed')
+    }
+});
+
 /* ------ Hero SlideShow initialisation ------------*/
 
 bfSlick.assignCarousel('.js-bf-slideshow--hero-slideshow', {
@@ -19,13 +33,4 @@ bfSlick.assignCarousel('.js-bf-slideshow--hero-slideshow', {
     autoplay: true,
     autoplaySpeed: 5000,
     pauseOnDotsHover: true ,
-});
-
-/*-------- Apply reversed on navigation controls -------------*/
-
-$('.bf-slideshow__inner').on('afterChange', function(event, slick, currentSlide, nextSlide){
-    $('.bf-slideshow .bfslick__controls').removeClass('reversed')
-    if ($('.slick-current .bf-hero').hasClass('reversed')){
-        $('.bf-slideshow .bfslick__controls').addClass('reversed')
-    }
 });
