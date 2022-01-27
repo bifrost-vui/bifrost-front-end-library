13dd9a1146b85c5100a1e3b80ba1b9e1f489b5ea
bibifrost <baptiste.mac+videotron@gmail.com>
2022-01-27 22:12:19 +0100

    ds-234 add missing 40px spacing in documentation

--- projects/front-end-library/src/lib/_doc/styles/2-spacings/spacings.js
+++ projects/front-end-library/src/lib/_doc/styles/2-spacings/spacings.js
@@ -1,38 +1,35 @@
 const spacings = [
     {
         name            : 1,
-        valueREM        : .25,
-        valuePX         : 4,
+        valueREM        : .25
     },
     {
         name            : 2,
-        valueREM        : .5,
-        valuePX         : 8,
+        valueREM        : .5
     },
     {
         name            : 3,
-        valueREM        : 1,
-        valuePX         : 16,
+        valueREM        : 1
     },
     {
         name            : 4,
-        valueREM        : 1.5,
-        valuePX         : 24,
+        valueREM        : 1.5
     },
     {
         name            : 5,
-        valueREM        : 3,
-        valuePX         : 48,
+        valueREM        : 2.5
     },
     {
         name            : 6,
-        valueREM        : 4.5,
-        valuePX         : 72,
+        valueREM        : 3
     },
     {
         name            : 7,
-        valueREM        : 6,
-        valuePX         : 96,
+        valueREM        : 4.5
+    },
+    {
+        name            : 8,
+        valueREM        : 6
     },
 ];
 
