8d02bc22b9926962d0d19716a805f0008af3024e
Nathalie Toussain <nathalie.toussaint@videotron.com>
2022-02-08 11:51:51 -0500

    DS-266 - input radio minimal component (input + label)

---
+++ projects/front-end-library/src/lib/components/input-radio/angular/input-radio.component.ts
@@ -0,0 +1,29 @@
+import { Component, ViewEncapsulation, OnInit, Input } from '@angular/core';
+
+@Component({
+  selector: 'bf-input-radio',
+  templateUrl: './input-radio.component.html',
+  // styleUrls: ['../scss/index.scss'],
+})
+export class InputRadioComponent implements OnInit {
+
+    constructor() {}
+
+    @Input() name           : string;
+    /** @required */
+    @Input() id             : string;
+    @Input() value          : string;
+    @Input() isChecked      : boolean;
+    @Input() isDisabled     : boolean;
+    @Input() isRequired     : boolean;
+    @Input() isInvalid      : boolean;
+    @Input() label          : string;
+    /** will be used if no label is defined */
+    @Input() ariaLabel      : string;
+    @Input() extraAttribute : string;
+    @Input() class          : string;
+
+    ngOnInit() {
+        console.log('InputRadio', this);
+    }
+}
