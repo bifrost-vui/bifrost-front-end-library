fb6aba63ec9ed0addc448e9c2609d9c2003e9b94
bibifrost <baptiste.mac+videotron@gmail.com>
2022-02-10 22:10:24 +0100

    ds-242 review indent

--- projects/front-end-library/src/lib/sections/text-image/angular/text-image.module.ts
+++ projects/front-end-library/src/lib/sections/text-image/angular/text-image.module.ts
@@ -2,8 +2,8 @@ import { NgModule } from '@angular/core';
 import { TextImageComponent } from './text-image.component';
 
 @NgModule({
-  declarations: [TextImageComponent],
-  imports: [],
-  exports: [TextImageComponent]
+    declarations: [TextImageComponent],
+    imports: [],
+    exports: [TextImageComponent]
 })
 export class TextImageModule { }
