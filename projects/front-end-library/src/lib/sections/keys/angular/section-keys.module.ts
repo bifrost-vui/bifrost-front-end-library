2e0f215226e28f4556ed85f1c11e8954a99c8c17
bibifrost <baptiste.mac+videotron@gmail.com>
2022-02-10 15:08:38 +0100

    ds-253 indent

--- projects/front-end-library/src/lib/sections/keys/angular/section-keys.module.ts
+++ projects/front-end-library/src/lib/sections/keys/angular/section-keys.module.ts
@@ -2,8 +2,8 @@ import { NgModule } from '@angular/core';
 import { SectionKeysComponent } from './section-keys.component';
 
 @NgModule({
-  declarations: [SectionKeysComponent],
-  imports: [],
-  exports: [SectionKeysComponent]
+    declarations: [SectionKeysComponent],
+    imports: [],
+    exports: [SectionKeysComponent]
 })
 export class SectionKeysModule { }
