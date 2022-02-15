d37d48069516f30bc7eb34f687a631e2ca6caed1
bibifrost <baptiste.mac+videotron@gmail.com>
2022-02-15 16:05:35 +0100

    ds-181 indent

--- projects/showcase-angular/src/app/app.module.ts
+++ projects/showcase-angular/src/app/app.module.ts
@@ -1,20 +1,20 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { BadgeModule } from '@videotron-ui/bifrost-front-end-library';
+import { NgModule }             from '@angular/core';
+import { BrowserModule }        from '@angular/platform-browser';
+import { BadgeModule }          from '@videotron-ui/bifrost-front-end-library';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { AppRoutingModule }     from './app-routing.module';
+import { AppComponent }         from './app.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    BadgeModule,
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent
+    ],
+    imports: [
+        BrowserModule,
+        AppRoutingModule,
+        BadgeModule,
+    ],
+    providers: [],
+    bootstrap: [AppComponent]
 })
 export class AppModule { }
