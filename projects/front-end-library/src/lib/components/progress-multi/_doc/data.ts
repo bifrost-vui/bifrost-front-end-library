221863713abf54d66725a15afb8f9e73be90f2af
ThalieVideotron <nathalie.toussaint@videotron.com>
2022-02-18 12:16:16 -0500

    DS-290- Progress-Multi Component

---
+++ projects/front-end-library/src/lib/components/progress-multi/_doc/data.ts
@@ -0,0 +1,24 @@
+export const progressData = [
+    {
+        label: "Flex-go",
+        cssClass:"bg-flex-go",
+        percent:30,
+        currentValue:1,
+        minValue:2,
+        maxValue:2},
+    {
+        label: "Data-bloc",
+        cssClass:"bg-data-bloc",
+        percent:40,
+        currentValue:3,
+        minValue:4,
+        maxValue:4
+    },
+    {
+        label: "PPu",
+        cssClass:"bg-ppu",
+        percent:5,
+        currentValue:1,
+        minValue:"",
+        maxValue:""},
+];
