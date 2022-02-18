5412e6b6d985ddcdffeaff7bd4434e051607414d
ThalieVideotron <nathalie.toussaint@videotron.com>
2022-02-18 08:57:04 -0500

    DS-271 - progress - component creation en hit'n run

---
+++ projects/front-end-library/src/lib/components/progress/angular/progress.component.ts
@@ -0,0 +1,26 @@
+import { Component, ViewEncapsulation, OnInit, Input } from '@angular/core';
+
+/**
+ * Description of the component Progress
+ *
+ */
+
+@Component({
+  selector: 'bf-progress',
+  templateUrl: './progress.component.html',
+  // styleUrls: ['../scss/index.scss'],
+})
+export class ProgressComponent implements OnInit {
+    constructor() {}
+
+
+    @Input() progressPercentValue   : number;
+    @Input() progressValue          : number;
+    @Input() progressColor          : 'default'|'success'|'warning'|'danger';
+    @Input() progressBarClass       : string;
+    @Input() class                  : string;
+
+    ngOnInit() {
+        console.log('Progress', this);
+    }
+}
