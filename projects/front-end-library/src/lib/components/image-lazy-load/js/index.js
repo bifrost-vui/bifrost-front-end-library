8c40ed43cf97a1b346156fc82203bc099b69c2a3
ThalieVideotron <nathalie.toussaint@videotron.com>
2022-02-22 15:55:35 -0500

    DS-278 - Image Lasy Loading

---
+++ projects/front-end-library/src/lib/components/image-lazy-load/js/index.js
@@ -0,0 +1,12 @@
+const allImages = document.getElementsByTagName('img');
+
+for (let i = 0; i < allImages.length; i++) {
+    if (allImages[i].getAttribute('data-src')) {
+        let img = new Image();
+        img.src = allImages[i].getAttribute('data-src');
+        img.onload = function () {
+            allImages[i].setAttribute('src', allImages[i].getAttribute('data-src'));
+            allImages[i].classList.add('loaded');
+        }
+    }
+}
