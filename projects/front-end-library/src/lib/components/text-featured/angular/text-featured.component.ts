16cf1aaeec574c75c6b0b80e53c27a092aed3f24
ThalieVideotron <nathalie.toussaint@videotron.com>
2022-02-23 10:54:25 -0500

    DS-283-text-featured

---
+++ projects/front-end-library/src/lib/components/text-featured/angular/text-featured.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit, Input } from '@angular/core';
+
+@Component({
+    selector: 'bf-text-featured',
+    templateUrl: './text-featured.component.html',
+})
+export class TextFeaturedComponent implements OnInit {
+    constructor() {}
+
+    @Input() spacingLeft: number;
+    @Input() class      : string;
+
+    ngOnInit() {
+        console.log('TextFeatured', this);
+    }
+}
