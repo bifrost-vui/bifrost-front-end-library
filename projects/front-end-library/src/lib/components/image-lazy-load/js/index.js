057499582cde33cec67ceabef73a4208ea234f37
ThalieVideotron <nathalie.toussaint@videotron.com>
2022-02-23 11:17:31 -0500

    DS-278 - image lazy load : cleaned up HTML file

--- projects/front-end-library/src/lib/components/image-lazy-load/js/index.js
+++ projects/front-end-library/src/lib/components/image-lazy-load/js/index.js
@@ -10,3 +10,11 @@ for (let i = 0; i < allImages.length; i++) {
         }
     }
 }
+
+/**
+ * REMOVE COMMENT UPON REVIEW
+ * in the image.js file  : This script is called on the Load event but is not responding (for only one onload function is aloud)
+ * 2 solutions :
+ * the script is called only when the `Image Lazy Load` is used (present solution). meaning the Image.js script should be deleted to avoid code duplication
+ * the present script would be added to the onload.js. also meaning the image.js script should be deleted
+ * */
