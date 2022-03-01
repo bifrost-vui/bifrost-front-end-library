2b93db0883ca7bcf38be4d08b9b617d079ab103f
ThalieVideotron <nathalie.toussaint@videotron.com>
2022-03-01 09:56:57 -0500

    DS-300 - List component with divider

--- projects/front-end-library/src/lib/components/list/angular/list.component.ts
+++ projects/front-end-library/src/lib/components/list/angular/list.component.ts
@@ -4,28 +4,49 @@ import { Component, ViewEncapsulation, OnInit, Input } from '@angular/core';
  * Description of the component List
  *
  */
- 
+interface ILink {
+    label           : string;
+    href            : string;
+    iconName        : string;
+    iconePosition   : string;
+}
+
 @Component({
-  selector: 'bf-list',
-  templateUrl: './list.component.html',
-  // styleUrls: ['../scss/index.scss'],
+    selector: 'bf-list',
+    templateUrl: './list.component.html',
+    // styleUrls: ['../scss/index.scss'],
 })
 export class ListComponent implements OnInit {
     constructor() {}
 
     @Input() title              : string;
     @Input() titleTag           : string;
-    /** Expected format: 
-    ```
-    [{
-        label   : "Label",
-        iconName: "placeholder"
-    }]
-    ```
-    */
-    @Input() content            : object;
+    @Input() titleClass         : string;
+    /** Displays on the right size of the title */
+    @Input() link               : ILink;
+    /** Displays on the right size of the title if no link is defined */
+    @Input() description        : string;
+    /** Displays bellow the title and may contain HTML tags */
+    @Input() details            : string;
+    /** Expected format:
+     <pre style='font-size: .75rem; padding: 0 1rem; marigin:0; background-color: #F8F8F8'>[{
+     label: "Label",
+     iconName: "placeholder"
+     link: {
+          label: "link",
+          href: "https://www.example.com/"
+          }
+     },
+     {...}]</pre>*/
 
+    @Input() content            : object;
+    /** When `true` creates a bullet list with a default `check` icon - you can still specify another iconName in `content[]` */
+    @Input() defaultBullet  : 'check' | 'dot' | null;
+    /** Adds a bottom border between list elements */
+    @Input() hasDivider         : boolean;
     @Input() reversed           : boolean;
+
+    @Input() contentClass       : string;
     @Input() class              : string;
 
     ngOnInit() {
