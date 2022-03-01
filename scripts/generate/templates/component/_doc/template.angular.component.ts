9749eadcc96e7d460cc5d4a75decae1efc9b1b99
bibifrost <baptiste.mac+videotron@gmail.com>
2022-03-01 16:17:12 +0100

    ds-289 add template for angular component

---
+++ scripts/generate/templates/component/_doc/template.angular.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit, Input } from '@angular/core';
+
+
+@Component({
+    selector: 'bf-%component-name%',
+    templateUrl: './template.angular.component.html',
+})
+export class %ComponentName%TemplateComponent implements OnInit {
+    constructor() {}
+
+    @Input() className      : string;
+
+    templates               : number[] = [1,2];
+
+    ngOnInit() {
+        console.log('%ComponentName%TemplateComponent', this);
+    }
+}
