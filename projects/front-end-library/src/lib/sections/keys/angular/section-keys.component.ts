9d98681c50c7c334619589c7a414e782e98bca79
bibifrost <baptiste.mac+videotron@gmail.com>
2022-03-07 16:07:41 +0100

    ds-253 add interfaces for IKey and IButton

--- projects/front-end-library/src/lib/sections/keys/angular/section-keys.component.ts
+++ projects/front-end-library/src/lib/sections/keys/angular/section-keys.component.ts
@@ -1,4 +1,14 @@
-import { Component, ViewEncapsulation, OnInit, Input, Output, EventEmitter} from '@angular/core';
+import { Component, OnInit, Input }     from '@angular/core';
+import { IButton }                      from '../../../components/button/angular/api.model';
+import { ILink }                        from '../../../components/link/angular/api.model';
+
+export interface IKey {
+    iconName        : string;
+    title           : string;
+    titleTag        : string;
+    description     : string;
+    link            : ILink;
+}
 
 @Component({
     selector: 'bf-section-keys',
@@ -10,8 +20,8 @@ export class SectionKeysComponent implements OnInit {
     @Input() title          : string;
     @Input() titleTag       : string;
     @Input() description    : string;
-    @Input() keys           : object[];
-    @Input() buttons        : object[];
+    @Input() keys           : IKey[];
+    @Input() buttons        : IButton[];
     @Input() align          : 'left' | 'center';
     @Input() background     : 'ground' | 'underground' | 'highlight';
 
