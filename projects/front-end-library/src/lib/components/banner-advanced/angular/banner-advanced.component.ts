289c643036457b457fb29547ace8f8a517cad37d
ThalieVideotron <nathalie.toussaint@videotron.com>
2022-03-10 11:34:34 -0500

    DS-309 - Banner Advanced - changed `subtitle` to `description`

--- projects/front-end-library/src/lib/components/banner-advanced/angular/banner-advanced.component.ts
+++ projects/front-end-library/src/lib/components/banner-advanced/angular/banner-advanced.component.ts
@@ -9,11 +9,11 @@ export class BannerAdvancedComponent implements OnInit {
     constructor() {}
 
     @Input() title      : string;
-    @Input() subtitle   : string;
+    @Input() description: string;
     /** Expected format: [{name: string, label: string}, {...}] - a maximum of 3 icons is accepted*/
-    @Input() icons   : object[];
+    @Input() icons      : object[];
 
-    @Input() class  : string;
+    @Input() class      : string;
 
     ngOnInit() {
         console.log('BannerAdvanced', this);
