056ab050573ace299f719172c14eafbe44ddb53d
ThalieVideotron <nathalie.toussaint@videotron.com>
2022-03-28 14:31:41 -0400

    DS-320 Modal - creation

---
+++ projects/front-end-library/src/lib/components/modal/angular/modal.component.ts
@@ -0,0 +1,42 @@
+import {Component, OnInit, Input} from '@angular/core';
+
+@Component({
+    selector: 'bf-modal',
+    templateUrl: './modal.component.html',
+})
+
+export class ModalComponent implements OnInit {
+    constructor() {
+    }
+
+    @Input() id: string;
+    @Input() modalId: string;
+    @Input() modalSize: 'sm' | 'md' | 'lg';
+    @Input() modalAnimation: string;
+    @Input() modalLabelledBy: string;
+    @Input() extraAttribute:string;
+
+    @Input() bgImage: string;
+    @Input() bgLgImage: string;
+    @Input() bgMdImage: string;
+
+    @Input() modal_header: string | 'TwigBlock';
+    @Input() modalHeaderClass: string;
+    @Input() modalTitle: string;
+    @Input() modalSubtitle: string;
+    @Input() modalContentCLass: string;
+
+    /** The Modal content takes full height */
+    @Input() modal_body_subcontainer: string | 'TwigBlock';
+    /** Makes the content of the modal scrollable */
+    @Input() modal_body_scroll_container: string | 'TwigBlock';
+
+    @Input() modalBudy: string;
+    @Input() modal_footer: string | 'TwigBlock';
+    /**Twig only*/
+    @Input() class: string;
+
+    ngOnInit() {
+        console.log('Modal', this);
+    }
+}
