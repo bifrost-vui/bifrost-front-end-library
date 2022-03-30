d4cf851f66ac0921c5b9cc53339f0c1222680226
ThalieVideotron <nathalie.toussaint@videotron.com>
2022-03-30 14:19:43 -0400

    DS-290 multiprogress - empty lines, indents and general formatting

--- projects/front-end-library/src/lib/components/progress-multi/_doc/data.ts
+++ projects/front-end-library/src/lib/components/progress-multi/_doc/data.ts
@@ -8,19 +8,19 @@ export const progressData = [
         maxValue: 2
     },
     {
-        label: "Data-bloc",
-        cssClass:"bg-data-bloc",
-        percent:22.5,
-        currentValue:3,
-        minValue:0,
-        maxValue:4
+        label: 'Data-bloc',
+        cssClass: 'bg-data-bloc',
+        percent: 22.5,
+        currentValue: 3,
+        minValue: 0,
+        maxValue: 4
     },
     {
-        label: "PPu",
-        cssClass:"bg-ppu progress-bar-striped progress-bar-animated",
-        percent:20,
-        currentValue:2,
-        minValue:0,
-        maxValue:4
+        label: 'PPu',
+        cssClass: 'bg-ppu progress-bar-striped progress-bar-animated',
+        percent: 20,
+        currentValue: 2,
+        minValue: 0,
+        maxValue: 4
     },
 ];
