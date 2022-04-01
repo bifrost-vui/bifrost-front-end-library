7a21989a081bdad89bcdc69604d18dd55c904e86
ThalieVideotron <nathalie.toussaint@videotron.com>
2022-04-01 13:37:27 -0400

    DS-266 input-radio - code style + fixed issues

--- projects/front-end-library/src/lib/components/input-radio/angular/input-radio.component.ts
+++ projects/front-end-library/src/lib/components/input-radio/angular/input-radio.component.ts
@@ -9,20 +9,20 @@ export class InputRadioComponent implements OnInit {
 
     constructor() {}
 
-    @Input() name           : string;
+    @Input() name: string;
     /** @required */
-    @Input() id             : string;
-    @Input() value          : string;
-    @Input() isChecked      : boolean;
-    @Input() isDisabled     : boolean;
-    @Input() isRequired     : boolean;
-    @Input() isInvalid      : boolean;
-    @Input() label          : string;
-    @Input() labelClass     : string;
+    @Input() id: string;
+    @Input() value: string;
+    @Input() isChecked: boolean;
+    @Input() isDisabled: boolean;
+    @Input() isRequired: boolean;
+    @Input() isInvalid: boolean;
+    @Input() label: string;
+    @Input() labelClass: string;
     /** Needs to be defined if no label is defined */
-    @Input() ariaLabel      : string;
-    @Input() extraAttribute : string;
-    @Input() class          : string;
+    @Input() ariaLabel: string;
+    @Input() extraAttribute: string;
+    @Input() class: string;
 
     ngOnInit() {
         console.log('InputRadio', this);
