51e08afe3b541ed1060ac6055e868b770a6fe12e
ThalieVideotron <nathalie.toussaint@videotron.com>
2022-04-08 09:48:47 -0400

    DS-324- callout - fix data.ts formatting

--- projects/front-end-library/src/lib/components/callout/_doc/data.ts
+++
@@ -1,7 +0,0 @@
-export const contentDemo =
-    "<ul class='bf-text-display-sub-medium p-3 pb-0 mb-0'>"+
-    "<li>Lorem ipsum dolor sit amet.</li>"+
-    "<li>Integer tristique mi ut ornare malesuada.</li>"+
-    "<li>Aenean ullamcorper neque at luctus accumsan.</li>"+
-    "<li>Vestibulum ultricies ullamcorper sem.</li>"+
-    "</ul>";
