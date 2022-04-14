4867764c5db0b44d5622aa0c19f8018c39b10d5c
ThalieVideotron <nathalie.toussaint@videotron.com>
2022-04-14 16:31:24 -0400

    DS-267 - Input Switch

--- projects/front-end-library/src/lib/components/input-switch/angular/input-switch.component.ts
+++ projects/front-end-library/src/lib/components/input-switch/angular/input-switch.component.ts
@@ -10,14 +10,15 @@ export class InputSwitchComponent implements OnInit {
     constructor() {}
 
 
-    @Input() id             : string;
-    @Input() name           : string;
-    /** Make sure to define aria-label (`labelOn` and `labelOff` are not HTML labels) */
-    @Input() ariaLabel      : string;
-    @Input() isDisabled     : boolean;
-    @Input() isChecked      : boolean;
-    @Input() extraAttribute : string;
-    @Input() class          : string;
+    @Input() id: string;
+    @Input() name: string;
+    /** the label can either be a string or an object: { text, class, position (left|right) }. The label default position is left */
+    @Input() label: string|object;
+    @Input() ariaLabel: string;
+    @Input() isDisabled: boolean;
+    @Input() isChecked: boolean;
+    @Input() extraAttribute: string;
+    @Input() class: string;
 
     ngOnInit() {
         console.log('InputSwitch', this);
