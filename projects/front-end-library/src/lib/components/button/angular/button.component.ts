c55b1b921ad708844c38ad55cf909783ceea901a
Nathalie Toussaint <82388009+ThalieVideotron@users.noreply.github.com>
2022-04-19 09:24:36 -0400

    Update projects/front-end-library/src/lib/components/button/angular/button.component.ts
    Co-authored-by: bibifrost <93746960+bibifrost@users.noreply.github.com>

--- projects/front-end-library/src/lib/components/button/angular/button.component.ts
+++ projects/front-end-library/src/lib/components/button/angular/button.component.ts
@@ -8,40 +8,40 @@ import { Component, ViewEncapsulation, OnInit, Input, Output, EventEmitter} from
 export class ButtonComponent implements OnInit {
     constructor() {}
 
+    @Input() label: string;
     @Input() hierarchy: null | 'primary' | 'primary-alt' | 'secondary' | 'tertiary';
-    @Input() fullWidth: boolean;
-
     /** A button will become a link if `href` is defined as long as it is not Disabled or a submit button. */
     @Input() href: string;
-    @Input() label: string;
-    @Input() ariaLabel: string;
+    @Input() fullWidth: boolean;
+    @Input() class: string;
+    @Input() id: string;
+    @Input() extraAttribute: string;
 
+    @Input() isDisabled: boolean;
+    @Input() isAutofocus: boolean;
+    @Input() isLoading: boolean;
+    @Input() hasLoading: boolean;
+    
     @Input() iconName: string;
     @Input() iconPosition: null | 'left' | 'right';
     /** Changing the size of the icon is not recommended. */
     @Input() iconSize: string;
-
+    
+    @Input() ariaLabel: string;
+    @Input() ariaControls: string;
+    @Input() ariaExpanded: string;
+    
     @Input() dataDismiss: string;
     @Input() dataToggle: string;
     @Input() dataTarget: string;
-    @Input() ariaControls: string;
-    @Input() ariaExpanded: string;
-    @Input() extraAttribute: string;
-    @Input() class: string;
-
-    @Input() id: string;
+    
+    @Input() isSubmit: boolean;
     @Input() formId: string;
     @Input() formAction: string;
     @Input() formEncryptionType: null | 'application/x-www-form-urlencoded' |'multipart/form-data' | 'text/plain';
     @Input() formTarget: null | '_self' | '_blank' | '_parent' | '_top';
     @Input() formMethod: string;
     @Input() formNoValidate: boolean;
-
-    @Input() isDisabled: boolean;
-    @Input() isSubmit: boolean;
-    @Input() isAutofocus: boolean;
-    @Input() isLoading: boolean;
-    @Input() hasLoading: boolean;
 
     ngOnInit() {
         console.log('button', this);
