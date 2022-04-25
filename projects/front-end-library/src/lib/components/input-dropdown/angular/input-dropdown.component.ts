b93d7b52f11dce791ffd39faade91de69957b497
bibifrost <baptiste.mac+videotron@gmail.com>
2022-04-26 00:54:08 +0200

    ds-224 improve dropdown with native style

--- projects/front-end-library/src/lib/components/input-dropdown/angular/input-dropdown.component.ts
+++ projects/front-end-library/src/lib/components/input-dropdown/angular/input-dropdown.component.ts
@@ -7,22 +7,28 @@ import { Component, OnInit, Input } from '@angular/core';
 export class InputDropdownComponent implements OnInit {
     constructor() {}
 
-    /** options: [ { label, value, isSelected } ] */
+    /** Options required: `[{ label, value }]` If `isNative` is false, options can pass an array of [Buttons](?path=/docs/components-button--drupal). */
     @Input() options                : Array<object>[];
+    @Input() isNative               : boolean;
 
-    @Input() inputId                : string;
-    @Input() inputExtraAttribute    : string;
+    // Inputs commons to all inputs:
+    @Input() value                  : string;
+    @Input() placeholder            : string;
     @Input() label                  : string;
     /** See [Button Icon](?path=/docs/components-button-icon--drupal) component API. */
     @Input() labelButtonIcon        : object;
     @Input() description            : string;
-    @Input() value                  : string; // ?
-    @Input() placeholder            : string;
     @Input() helperText             : string;
     @Input() errorMessage           : string;
+    /** `isRequired` works only if `isNative` is true. */
+    @Input() isRequired             : boolean;
+    @Input() isAutofocus            : boolean;
     @Input() isInvalid              : boolean;
     @Input() isDisabled             : boolean;
     @Input() isReadOnly             : boolean;
+    @Input() inputId                : string;
+    @Input() inputName              : string;
+    @Input() inputExtraAttribute    : string;
 
     @Input() reversed               : boolean;
     @Input() class                  : string;
