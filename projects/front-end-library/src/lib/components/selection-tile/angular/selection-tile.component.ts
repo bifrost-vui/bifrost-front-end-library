c759f6ca24b304b5cf26046c3471918ecdb6ba01
ThalieVideotron <nathalie.toussaint@videotron.com>
2022-05-03 19:43:13 -0400

    DS-327 - Selection tile with footer

--- projects/front-end-library/src/lib/components/selection-tile/angular/selection-tile.component.ts
+++ projects/front-end-library/src/lib/components/selection-tile/angular/selection-tile.component.ts
@@ -1,5 +1,7 @@
 import { Component, ViewEncapsulation, OnInit, Input } from '@angular/core';
-import { Url } from 'url';
+import { ILink } from '../../link/angular/api.model';
+import {HTMLElement} from '@webcomponents/custom-elements/src/Patch/Native';
+
 
 @Component({
   selector: 'bf-selection-tile',
@@ -10,30 +12,40 @@ export class SelectionTileComponent implements OnInit {
     constructor() {}
 
     /** Class override */
-    @Input() class          : string;
-    @Input() type           : 'checkbox'|'radio';
-    @Input() inputId        : string;
-    @Input() value          : string;
-    @Input() ariaLabel      : string;
-    @Input() name           : string;
+    @Input() class: string;
+    @Input() type: 'checkbox'|'radio';
+    @Input() inputId: string;
+    @Input() value: string;
+    @Input() ariaLabel: string;
+    @Input() name: string;
     /** extra attribute for the radio/checkbox input */
-    @Input() extraAttribute : string
-    @Input() orientation    : 'horizontal'|'vertical';
-    /**  Tile  fits the content | Tile fits the container  */
-    @Input() fit            : 'content'|'container';
-    @Input() title          : string;
-    @Input() description    : string;
-    @Input() info           : string;
-    /**{ src : string, alt : string, class : string } */
-    @Input() image          : object;
-    @Input() iconName       : string;
-    @Input() reversed       : boolean;
-    @Input() isDisabled     : boolean;
-    @Input() isSelected     : boolean;
-    @Input() isInvalid      : boolean;
-    @Input() errorMessage   : string;
-    @Input() isRequired     : boolean;
-    @Input() content        : string | 'TwigBlock';
+    @Input() extraAttribute: string
+    @Input() orientation: 'horizontal'|'vertical';
+    /**  Tile  fits the content | Tile fits the container (up to 20.5rem)  */
+    @Input() fit: 'content'|'container';//hug content vs max width
+    @Input() title: string;
+    @Input() description: string;
+    /** Only available if the tile fits container and is vertical */
+    @Input() info: string;
+    /**{ src: string, alt: string, class: string } */
+    @Input() image: object;
+    @Input() iconName: string;
+    @Input() reversed: boolean;
+    @Input() isDisabled: boolean;
+    @Input() isSelected: boolean;
+    @Input() isInvalid: boolean;
+    @Input() errorMessage: string;
+    @Input() isRequired: boolean;
+    @Input() content: HTMLElement | 'TwigBlock';
+    /** For horizontal tile only - Can contain HTML */
+    @Input() footerText: HTMLElement;
+    /** for horizontal tile only - see [Link] */
+    @Input() footerLink: ILink;
+    /** For horizontal tile only. Expected: {src: string, alt:string}*/
+    @Input() footerImage: object;
+    /** For horizontal tile only.*/
+    @Input() footerIconName: string;
+
 
     ngOnInit() {
         console.log('SelectionTile', this);
