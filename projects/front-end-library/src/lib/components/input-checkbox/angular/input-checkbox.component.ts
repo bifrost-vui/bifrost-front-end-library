0c37b3cec6bff0871df687a885e5fc85558d5c16
ThalieVideotron <nathalie.toussaint@videotron.com>
2022-05-04 10:25:09 -0400

    DS-339 - fixe Checkbox - part 1 : the checkbox stories

--- projects/front-end-library/src/lib/components/input-checkbox/angular/input-checkbox.component.ts
+++ projects/front-end-library/src/lib/components/input-checkbox/angular/input-checkbox.component.ts
@@ -11,6 +11,8 @@ export class InputCheckboxComponent implements OnInit {
 
     /** @required */
     @Input() id: string;
+    /** will transform the checkbox into a radio button displaying a checkmark when checked */
+    @Input() type: 'checkbox' | 'radio'
     @Input() name: string;
     @Input() value: string;
     @Input() isChecked: boolean;
@@ -20,7 +22,6 @@ export class InputCheckboxComponent implements OnInit {
     @Input() extraAttribute: string;
     @Input() label: string;
     @Input() labelClass: string;
-    @Input() labelExtraAttribute: string;
     /** Needs to be defined if no label is defined */
     @Input() ariaLabel: string;
     @Input() class: string;
