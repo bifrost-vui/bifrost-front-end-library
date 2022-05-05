581eb7013f9249642782175f643ce26bcb1a4b26
bibifrost <baptiste.mac+videotron@gmail.com>
2022-05-05 16:30:01 +0200

    ds-254 improve button nav

--- projects/front-end-library/src/lib/sections/buttons-nav/angular/section-buttons-nav.component.ts
+++ projects/front-end-library/src/lib/sections/buttons-nav/angular/section-buttons-nav.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, Input } from '@angular/core';
-
+import { IButtonNav } from '../../../components/button-nav/angular/api.model';
+import { ILink } from '../../../components/link/angular/api.model';
 @Component({
     selector: 'bf-section-buttons-nav',
     templateUrl: './section-buttons-nav.component.html',
@@ -7,18 +8,25 @@ import { Component, OnInit, Input } from '@angular/core';
 export class SectionButtonsNavComponent implements OnInit {
     constructor() {}
 
-    @Input() title          : string;
-    @Input() titleTag       : string;
-    @Input() buttons        : object[];
-    @Input() isFeatured     : boolean;
-    @Input() rightSection   : object;
-    @Input() background     : 'ground' | 'underground' | 'highlight';
+    @Input() title: string;
+    @Input() titleTag: string;
+    @Input() buttons: IButtonNav[];
+    @Input() isFeatured: boolean;
+    @Input() background: 'ground' | 'underground' | 'highlight';
+    /** Add a section at the right of the section button nav. `{ title, description, link }` */
+    @Input() rightSection: IRightSection;
 
-    @Input() extraAttribute : string;
-    @Input() class          : string;
-    @Input() reversed       : boolean;
+    @Input() extraAttribute: string;
+    @Input() class: string;
+    @Input() reversed: boolean;
 
     ngOnInit() {
         console.log('section buttons nav', this);
     }
+}
+
+interface IRightSection {
+    title: string;
+    description: string;
+    link: ILink;
 }
