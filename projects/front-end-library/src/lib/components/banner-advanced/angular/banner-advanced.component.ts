0adf0985c9d21f7c3e99fbef306f1f7ea9f4c244
ThalieVideotron <nathalie.toussaint@videotron.com>
2022-05-06 06:42:34 -0400

    DS-310-footer in banner - footer refactor & fix HTML content

--- projects/front-end-library/src/lib/components/banner-advanced/angular/banner-advanced.component.ts
+++ projects/front-end-library/src/lib/components/banner-advanced/angular/banner-advanced.component.ts
@@ -1,6 +1,4 @@
 import { Component, OnInit, Input } from '@angular/core';
-import { IInputSwitch } from '../../input-switch/angular/api.model';
-
 
 @Component({
     selector: 'bf-banner-advanced',
@@ -9,13 +7,12 @@ import { IInputSwitch } from '../../input-switch/angular/api.model';
 export class BannerAdvancedComponent implements OnInit {
     constructor() {}
 
-    @Input() title      : string;
+    @Input() title: string;
     @Input() description: string;
     /** Expected format: [{name: string, label: string}, {...}]. A maximum of 3 icons is accepted */
     @Input() icons: object[];
-    @Input() footer: HTMLElement | 'TwigBlock';
+    @Input() footer: object | HTMLElement | 'TwigBlock';
     /** See [Input Switch](/?path=/story/components-input-switch--drupal) */
-    @Input() inputSwitch: IInputSwitch;
     @Input() class: string;
     ngOnInit() {
         console.log('BannerAdvanced', this);
