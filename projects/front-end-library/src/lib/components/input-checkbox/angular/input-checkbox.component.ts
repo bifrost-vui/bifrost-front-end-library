9ed14cdc5fe86e38392ed569177ab3be8b0e848e
bibifrost <baptiste.mac+videotron@gmail.com>
2022-05-06 23:44:20 +0200

    ds-333 add input checkbox

---
+++ projects/front-end-library/src/lib/components/input-checkbox/angular/input-checkbox.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit, Input } from '@angular/core';
+import { IInputCheckboxOption } from '../../input-checkbox-option/angular/api.model';
+import { ISelectionTile } from '../../selection-tile/angular/api.model';
+
+@Component({
+    selector: 'bf-input-checkbox',
+    templateUrl: './input-checkbox.component.html',
+})
+export class InputCheckboxComponent implements OnInit {
+    constructor() {}
+
+    @Input() options: IInputCheckboxOption[] | ISelectionTile[];
+    /** Transform options as tiles. Use [Selection Tile API](?path=/story/components-selection-tile--drupal-selection-tile-vertical). */
+    @Input() isOptionTiles: boolean;
+    @Input() label: string;
+    @Input() description: string;
+    @Input() errorMessage: string;
+
+    @Input() class: string;
+
+    ngOnInit() {
+        console.log('InputCheckbox', this);
+    }
+}
