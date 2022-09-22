962467475caa3782d2ab3a85eb28f6affc0ad281
jonathan.lefebvreinf@videotron.com <jonathan.lefebvreinf@videotron.com>
2022-09-22 01:49:49 -0400

    DS-388: fix build error, fix doc, fix unsued argtypes

--- projects/front-end-library/src/lib.module.ts
+++ projects/front-end-library/src/lib.module.ts
@@ -2,8 +2,8 @@ import { NgModule } from '@angular/core';
 import { IconModule } from './lib/components/icon/angular/icon.module';
 
 @NgModule({
-  declarations: [IconModule],
-  imports: [
+  declarations: [],
+  imports: [IconModule
   ],
   exports: [IconModule],
 })
