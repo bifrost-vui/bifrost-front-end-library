403952c2ec8bc4d1a48dede3602774420095cd30
jonathan.lefebvreinf@videotron.com <jonathan.lefebvreinf@videotron.com>
2022-12-10 09:26:01 -0500

    DS-374: refactor Button style

--- projects/front-end-library/src/lib/components/button/angular/button.component.ts
+++ projects/front-end-library/src/lib/components/button/angular/button.component.ts
@@ -1,4 +1,4 @@
-import { Component, ViewEncapsulation, OnInit, Input, Output, EventEmitter} from "@angular/core";
+import { Component, ViewEncapsulation, OnInit, Input, Output, EventEmitter } from '@angular/core';
 
 /**
  * API is the same between **Angular** and **Drupal**.
@@ -6,7 +6,7 @@ import { Component, ViewEncapsulation, OnInit, Input, Output, EventEmitter} from
  */
 
 @Component({
-    selector: "bf-button",
+    selector: 'bf-button',
     templateUrl: './button.component.html',
     // styleUrls: ['../common/style.scss'],
 })
@@ -14,34 +14,34 @@ export class ButtonComponent implements OnInit {
     constructor() {}
 
     @Input() id: string;
-    @Input() label          : string;
+    @Input() label: string;
     /**
         A button will become a link if `href` is defined.
     */
-    @Input() href           : string;
+    @Input() href: string;
     @Input() hierarchy: null | 'primary' | 'primary-alt' | 'secondary' | 'tertiary';
-    @Input() isDisabled     : boolean;
-    @Input() hasLoading     : boolean;
-    @Input() isLoading      : boolean;
-    @Input() fullWidth      : boolean;
+    @Input() isDisabled: boolean;
+    @Input() hasLoading: boolean;
+    @Input() isLoading: boolean;
+    @Input() fullWidth: boolean;
 
-    @Input() iconName       : string;
+    @Input() iconName: string;
     /** only have effect if `iconName` is filled */
-    @Input() iconPosition   : null | 'left' | 'right';
+    @Input() iconPosition: null | 'left' | 'right';
 
-    @Input() dataDismiss    : string;
-    @Input() dataToggle     : string;
-    @Input() dataTarget     : string;
-    @Input() ariaControls   : string;
-    @Input() ariaExpanded   : string;
-    @Input() ariaLabel      : string;
-    @Input() extraAttribute : string;
-    @Input() class          : string;
+    @Input() dataDismiss: string;
+    @Input() dataToggle: string;
+    @Input() dataTarget: string;
+    @Input() ariaControls: string;
+    @Input() ariaExpanded: string;
+    @Input() ariaLabel: string;
+    @Input() extraAttribute: string;
+    @Input() class: string;
 
     @Input() isSubmit: boolean;
     @Input() formId: string;
     @Input() formAction: string;
-    @Input() formEncryptionType: null | 'application/x-www-form-urlencoded' |'multipart/form-data' | 'text/plain';
+    @Input() formEncryptionType: null | 'application/x-www-form-urlencoded' | 'multipart/form-data' | 'text/plain';
     @Input() formTarget: null | '_self' | '_blank' | '_parent' | '_top';
     @Input() formMethod: string;
     @Input() formNoValidate: boolean;
