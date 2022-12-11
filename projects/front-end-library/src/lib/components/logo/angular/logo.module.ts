ae38ccee4fee284467a61370651615e13130ce22
jonathan.lefebvreinf@videotron.com <jonathan.lefebvreinf@videotron.com>
2022-12-11 10:26:42 -0500

    DS-374: refactor Logo style

--- projects/front-end-library/src/lib/components/logo/angular/logo.module.ts
+++ projects/front-end-library/src/lib/components/logo/angular/logo.module.ts
@@ -2,8 +2,8 @@ import { NgModule } from '@angular/core';
 import { LogoComponent } from './logo.component';
 
 @NgModule({
-  declarations: [LogoComponent],
-  imports: [],
-  exports: [LogoComponent]
+    declarations: [LogoComponent],
+    imports: [],
+    exports: [LogoComponent],
 })
-export class LogoModule { }
+export class LogoModule {}
