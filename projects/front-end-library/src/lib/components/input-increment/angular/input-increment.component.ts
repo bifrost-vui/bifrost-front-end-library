b91ccacbfbff6a3295f3a7f69e8f03991299f1b0
jonathan.lefebvreinf@videotron.com <jonathan.lefebvreinf@videotron.com>
2022-12-11 08:00:54 -0500

    DS-374: refactor input-increment style

--- projects/front-end-library/src/lib/components/input-increment/angular/input-increment.component.ts
+++ projects/front-end-library/src/lib/components/input-increment/angular/input-increment.component.ts
@@ -1,22 +1,22 @@
-import { Component, ViewEncapsulation, OnInit, Input, Output, EventEmitter} from '@angular/core';
+import { Component, ViewEncapsulation, OnInit, Input, Output, EventEmitter } from '@angular/core';
 
 @Component({
-  selector: 'bf-input-increment',
-  templateUrl: './input-increment.component.html',
+    selector: 'bf-input-increment',
+    templateUrl: './input-increment.component.html',
 })
 export class InputIncrementComponent implements OnInit {
     constructor() {}
 
-    @Input() inputId      : string;
-    @Input() value        : number;
-    @Input() min          : number;
-    @Input() max          : number;
-    @Input() ariaLabel    : string;
-    @Input() isReadOnly   : boolean;
-    @Input() isDisabled   : boolean;
-    @Input() isRequired   : boolean;
-    @Input() errorMessage : string;
-    @Input() class        : string;
+    @Input() inputId: string;
+    @Input() value: number;
+    @Input() min: number;
+    @Input() max: number;
+    @Input() ariaLabel: string;
+    @Input() isReadOnly: boolean;
+    @Input() isDisabled: boolean;
+    @Input() isRequired: boolean;
+    @Input() errorMessage: string;
+    @Input() class: string;
 
     ngOnInit() {
         console.log('InputIncrement', this);
