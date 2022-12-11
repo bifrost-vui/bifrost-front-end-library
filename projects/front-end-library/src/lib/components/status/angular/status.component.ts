de8237bebc01816be2947aa996830ff077ca4b86
jonathan.lefebvreinf@videotron.com <jonathan.lefebvreinf@videotron.com>
2022-12-11 15:17:29 -0500

    DS-374: refactor Status style

--- projects/front-end-library/src/lib/components/status/angular/status.component.ts
+++ projects/front-end-library/src/lib/components/status/angular/status.component.ts
@@ -1,16 +1,16 @@
 import { Component, ViewEncapsulation, OnInit, Input, Output, EventEmitter } from '@angular/core';
 
 @Component({
-  selector: 'bf-status',
-  templateUrl: './status.component.html',
-  // styleUrls: ['../scss/index.scss'],
+    selector: 'bf-status',
+    templateUrl: './status.component.html',
+    // styleUrls: ['../scss/index.scss'],
 })
 export class StatusComponent implements OnInit {
     constructor() {}
 
-    @Input() label : string;
-    @Input() color : string;
-    @Input() class : string;
+    @Input() label: string;
+    @Input() color: string;
+    @Input() class: string;
 
     ngOnInit() {
         console.log('status', this);
