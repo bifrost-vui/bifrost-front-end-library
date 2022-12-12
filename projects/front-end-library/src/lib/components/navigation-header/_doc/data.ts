046a288c7cdf2bde3e13521d3f9a88af409d86d1
jonathan.lefebvreinf@videotron.com <jonathan.lefebvreinf@videotron.com>
2022-12-12 05:47:47 -0500

    DS-374: refactor Navigation-header style

--- projects/front-end-library/src/lib/components/navigation-header/_doc/data.ts
+++ projects/front-end-library/src/lib/components/navigation-header/_doc/data.ts
@@ -1,15 +1,15 @@
 export const topBar_items = [
-    { label: "Personal", href: "", class: "active" },
-    { label:"Business", href:"", class:"" },
+    { label: 'Personal', href: '', class: 'active' },
+    { label: 'Business', href: '', class: '' },
 ];
 
 export const topBar_secondaryItems = [
-    { label:"Webmail", href:"", class:"" },
-    { label:"illico.tv", href:"", class:"" },
-    { label:"Locate a store", href:"", class:"" }
+    { label: 'Webmail', href: '', class: '' },
+    { label: 'illico.tv', href: '', class: '' },
+    { label: 'Locate a store', href: '', class: '' },
 ];
 
 export const rigthIconLinks = [
-    { icon:"email", href:"", class:"" },
-    { icon:"pin", href:"", class:"" },
+    { icon: 'email', href: '', class: '' },
+    { icon: 'pin', href: '', class: '' },
 ];
