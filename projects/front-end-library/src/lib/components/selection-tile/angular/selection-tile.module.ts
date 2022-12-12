2c3daa2815c9dc6f898e9a9943b8194a1e5bf902
jonathan.lefebvreinf@videotron.com <jonathan.lefebvreinf@videotron.com>
2022-12-12 03:14:23 -0500

    DS-374: refactor Selection-Tile style

--- projects/front-end-library/src/lib/components/selection-tile/angular/selection-tile.module.ts
+++ projects/front-end-library/src/lib/components/selection-tile/angular/selection-tile.module.ts
@@ -2,9 +2,8 @@ import { NgModule } from '@angular/core';
 import { SelectionTileComponent } from './selection-tile.component';
 
 @NgModule({
-  declarations: [SelectionTileComponent],
-  imports: [
-  ],
-  exports: [SelectionTileComponent]
+    declarations: [SelectionTileComponent],
+    imports: [],
+    exports: [SelectionTileComponent],
 })
-export class SelectionTileModule { }
+export class SelectionTileModule {}
