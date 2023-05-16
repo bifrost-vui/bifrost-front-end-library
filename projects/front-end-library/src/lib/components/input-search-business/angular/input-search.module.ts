8e3d7eea407859b9586b7191300b72aa77f405b0
Maxim-Alexandre Lalancette <maxim-alexandre.lalancette@alithya.com>
2023-05-16 09:34:57 -0400

    DS-460: Add Input Search Business component
    - These are the original files from Input Search component
    - These files are slightly modified to changes paths and variables
    - This "new" component is added to support Navigation Header Business

---
+++ projects/front-end-library/src/lib/components/input-search-business/angular/input-search.module.ts
@@ -0,0 +1,9 @@
+import { NgModule } from '@angular/core';
+import { InputSearchBusinessComponent } from './input-search.component';
+
+@NgModule({
+    declarations: [InputSearchBusinessComponent],
+    imports: [],
+    exports: [InputSearchBusinessComponent],
+})
+export class InputSearchModule {}
