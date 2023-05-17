dc5feb069f0eb63be07e3291706118adfc891729
Maxim-Alexandre Lalancette <maxim-alexandre.lalancette@alithya.com>
2023-05-17 15:39:08 -0400

    DS-460: Adjust Button Icon angular files
    - Add an API model file to reuse in other components
    - Adjust component variables in alphabetical order

---
+++ projects/front-end-library/src/lib/components/button-icon/angular/api.model.ts
@@ -0,0 +1,16 @@
+export interface IButtonIcon {
+    ariaControls: string;
+    ariaExpanded: string;
+    ariaLabel: string;
+    badgeLabel: string;
+    buttonClass: string;
+    class: string;
+    dataDismiss: string;
+    dataTarget: string;
+    dataToggle: string;
+    extraAttribute: string | object;
+    href: string;
+    icon: string;
+    isDisabled: boolean;
+    label: string;
+}
