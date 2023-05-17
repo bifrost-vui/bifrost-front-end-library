10a57c793fd67a90defc9c473e1ca450f5400940
Maxim-Alexandre Lalancette <maxim-alexandre.lalancette@alithya.com>
2023-05-17 15:44:23 -0400

    DS-460: Refactor & optimize Input Search
    - Refactor to remove unused elements
    - Optimization of code
    - Add "Clear" button to clear field text
    - Add a "multiplier" param on SCSS calculateRem function

---
+++ projects/front-end-library/src/lib/components/input-search/_doc/doc.model.ts
@@ -0,0 +1,15 @@
+// Default Values
+export const ButtonSubmitDefaultValue = `{
+    buttonIcon: {
+        ariaLabel: "Search",
+        icon: "search",
+    },
+    isVisible: true,
+    position: "right",
+}`;
+
+export const InputDefaultValue = `{
+    id: "searchInput",
+    name: "searchInput",
+    placeholder: "What are you looking for?",
+}`;
