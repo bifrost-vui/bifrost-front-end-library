e5f0fe56adc9f641774c589c2b580146fd7c9162
Maxim-Alexandre Lalancette <maxim-alexandre.lalancette@alithya.com>
2023-06-22 12:23:57 -0400

    DS-460: Adjust header component docs - Part 2
    - Adjust documentation
    - Adjust API props

--- projects/front-end-library/src/lib/components/navigation-header/angular/api.component.ts
+++ projects/front-end-library/src/lib/components/navigation-header/angular/api.component.ts
@@ -1,14 +1,27 @@
 import { Component, Input } from '@angular/core';
-import { IAccount, IAccountLink, IHeaderLogo } from './api.model';
+import {
+    IAccount,
+    IAccountLink,
+    IDefaultMenuItem,
+    IHeaderLogo,
+    IMenuItem,
+    ITopBar,
+    ITopBarMenuItem,
+    ITopBarSection,
+} from './api.model';
 import { ILogo } from '../../../components/logo/angular/api.model';
 
 @Component({
     selector: '',
 })
 export class AccountAPI implements IAccount {
+    /** <span style="color: red;">__Required__</span> */
     @Input() firstName: string;
+    /** <span style="color: red;">__Required__</span> */
     @Input() lastName: string;
     /**
+     * <span style="color: red;">__Required__</span>
+     * <br><br>
      * See IAccountLink <a href="#iaccountlink-api" target="_self">API</a>
      *  and <a href="#iaccountlink-format" target="_self">expected format</a>.
      */
@@ -19,18 +32,186 @@ export class AccountAPI implements IAccount {
     selector: '',
 })
 export class AccountLinkAPI implements IAccountLink {
+    /** <span style="color: red;">__Required__</span> */
     @Input() href: string;
-    /** See [Icons - Interface List](/?path=/story/components-icon--drupal-interface) for icon names. */
+    /**
+     * <span style="color: red;">__Required__</span>
+     * <br><br>
+     * See [Icons - Interface List](/?path=/story/components-icon--drupal-interface) for icon names.
+     */
     @Input() icon: string;
+    /** <span style="color: orange;">__Optional__</span> */
     @Input() subtitle?: string;
+    /** <span style="color: red;">__Required__</span> */
     @Input() title: string;
 }
 
 @Component({
     selector: '',
 })
+export class DefaultMenuItemAPI implements IDefaultMenuItem {
+    /**
+     * <span style="color: orange;">__Optional__</span>
+     * <br><br>
+     * String of classes injected to this menu link.
+     * <br><br>
+     * For example, in Drupal, this is set in a `Class`
+     * text field in the menu item edition form.
+     */
+    @Input() class?: string;
+    /**
+     * <span style="color: orange;">__Optional__</span>
+     * <br><br>
+     * If there is no `href`, it will be a `<button>` instead of a `<a>`.
+     */
+    @Input() href?: string;
+    /**
+     * <span style="color: orange;">__Optional__</span>
+     * <br><br>
+     * If the menu item is active, meaning that:
+     * - the current page is the page this menu item goes to,
+     * - or the current page is a children / sub-page of that menu item.
+     */
+    @Input() isActive?: boolean = false;
+    /**
+     * <span style="color: orange;">__Optional__</span>
+     * <br><br>
+     * The active menu item is:
+     * - `true` = a clickable link
+     * - `false` = a non-clickable string of text
+     */
+    @Input() isActiveLinkClickable?: boolean = true;
+    /** <span style="color: red;">__Required__</span> */
+    @Input() label: string;
+    /** <span style="color: orange;">__Optional__</span> */
+    @Input() rel?: string;
+    /**
+     * <span style="color: orange;">__Optional__</span>
+     * <br><br>
+     * If the active menu item is a non-clickable string of text,
+     * then this prop will put a non-visible message before this menu item
+     * for screen readers to read. This is for accessibility purpose.
+     */
+    @Input() unclickableActiveMenuItemScreenReaderMessage?: string;
+}
+
+@Component({
+    selector: '',
+})
 export class HeaderLogoAPI implements IHeaderLogo {
-    href: string;
-    /** See Logo Component [API](/?path=/docs/components-logo--drupal). */
-    name: 'videotron' | 'business';
+    /** <span style="color: red;">__Required__</span> */
+    @Input() href: string;
+    /**
+     * <span style="color: red;">__Required__</span>
+     * <br><br>
+     * See Logo Component [API](/?path=/docs/components-logo--drupal).
+     */
+    @Input() name: 'videotron' | 'business';
+}
+
+@Component({
+    selector: '',
+})
+export class MenuItemAPI implements IMenuItem {
+    /**
+     * <span style="color: orange;">__Optional__</span>
+     * <br><br>
+     * CSS hexadecimal color code, ex.: `#FF0000`, that will
+     * put custom color style on this menu item link.
+     * <br><br>
+     * For example, in Drupal, this is set in a `Color`
+     * text field in the menu item edition form.
+     * <br><br>
+     * __Note__: In Storybook mock data, because of an issue,
+     * you can't put the `#` before the color. It will make the
+     * app crash.
+     */
+    @Input() color?: string;
+    /**
+     * <span style="color: orange;">__Optional__</span>
+     * <br><br>
+     * If this menu items have children to form a sub-menu,
+     * the prop `subItems` will be an array of objects using this current
+     * <a href="#imenuitem-api" target="_self">API</a> (IMenuItem).
+     */
+    @Input() subItems?: IMenuItem[];
+
+    // These following props are already available in "DefaultMenuItemAPI"
+
+    /** @ignore */
+    @Input() class?: string;
+    /** @ignore */
+    @Input() href?: string;
+    /** @ignore */
+    @Input() isActive?: boolean = false;
+    /** @ignore */
+    @Input() isActiveLinkClickable?: boolean = true;
+    /** @ignore */
+    @Input() label: string;
+    /** @ignore */
+    @Input() rel?: string;
+}
+
+@Component({
+    selector: '',
+})
+export class TopBarAPI implements ITopBar {
+    /**
+     * <span style="color: orange;">__Optional__</span>
+     * <br><br>
+     * __Left side__ of the top navigation bar
+     * <br><br>
+     * See ITopBarSection <a href="#itopbarsection-api" target="_self">API</a>
+     *  and <a href="#itopbarsection-format" target="_self">expected format</a>.
+     */
+    @Input() left?: ITopBarSection;
+    /**
+     * <span style="color: orange;">__Optional__</span>
+     * <br><br>
+     * __Right side__ of the top navigation bar
+     * <br><br>
+     * See ITopBarSection <a href="#itopbarsection-api" target="_self">API</a>
+     *  and <a href="#itopbarsection-format" target="_self">expected format</a>.
+     */
+    @Input() right?: ITopBarSection;
+}
+
+@Component({
+    selector: '',
+})
+export class TopBarMenuItemAPI implements ITopBarMenuItem {
+    // These following props are already available in "DefaultMenuItemAPI"
+
+    /** @ignore */
+    @Input() class?: string;
+    /** @ignore */
+    @Input() href?: string;
+    /** @ignore */
+    @Input() isActive?: boolean = false;
+    /** @ignore */
+    @Input() isActiveLinkClickable?: boolean = true;
+    /** @ignore */
+    @Input() label: string;
+    /** @ignore */
+    @Input() rel?: string;
+}
+
+@Component({
+    selector: '',
+})
+export class TopBarSectionAPI implements ITopBarSection {
+    /**
+     * <span style="color: orange;">__Optional__</span>
+     * <br><br>
+     * This is a description of the following menu for screen readers to read.
+     * This is for accessibility purpose only.
+     */
+    @Input() description?: string;
+    /**
+     * <span style="color: red;">__Required__</span>
+     * <br><br>
+     * See ITopBarMenuItem <a href="#itopbarmenuitem-api" target="_self">API</a>
+     *  and <a href="#itopbarmenuitem-format" target="_self">expected format</a>.
+     */
+    @Input() items: ITopBarMenuItem[];
 }
--- projects/front-end-library/src/lib/components/navigation-header/angular/header.component.ts
+++ projects/front-end-library/src/lib/components/navigation-header/angular/header.component.ts
@@ -1,5 +1,5 @@
 import { Component, ViewEncapsulation, OnInit, Input, Output, EventEmitter } from '@angular/core';
-import { IAccount, IHeaderLogo, IMenuItems, ITopBar } from './api.model';
+import { IAccount, IHeaderLogo, IMenuItem, ITopBar } from './api.model';
 @Component({
     selector: 'bf-header',
     templateUrl: './header.component.html',
@@ -8,64 +8,59 @@ export class HeaderComponent implements OnInit {
     constructor() {}
 
     /**
+     * <span style="color: orange;">__Optional__</span>
+     * <br><br>
      * See IAccount <a href="/?path=/docs/components-navigation-header-api--page#iaccount-api" target="_blank">API</a>
      *  and <a href="/?path=/docs/components-navigation-header-api--page#iaccount-format" target="_blank">expected format</a>.
      */
     @Input() accountData: IAccount = null;
     /**
+     * <span style="color: red;">__Required__</span>
+     * <br><br>
      * When anonymously browsing, meaning without being connected to an account,
      * this is the link on the "Sign In" button to access the login form.
      */
     @Input() accountSignInURL: string;
-    /** Display or not the top bar above main header bar */
+    /**
+     * <span style="color: orange;">__Optional__</span>
+     * <br><br>
+     * Display or not the top bar above main header bar
+     */
     @Input() hasTopBar: boolean = true;
+    /** <span style="color: orange;">__Optional__</span> */
     @Input() langcode: 'en' | 'fr' = 'en';
     /**
+     * <span style="color: red;">__Required__</span>
+     * <br><br>
      * See IHeaderLogo <a href="/?path=/docs/components-navigation-header-api--page#iheaderlogo-api" target="_blank">API</a>
      *  and <a href="/?path=/docs/components-navigation-header-api--page#iheaderlogo-format" target="_blank">expected format</a>.
      */
     @Input() logo: IHeaderLogo;
-    /** Expected format:
-     * <pre>
-     *     <code>
-     *         [
-     *             {
-     *                 "class": `string`,
-     *                 "color": `string`,
-     *                 "href": `string`,
-     *                 "label": `string`,
-     *                 "subItems": `IMenuItems[]`,
-     *             },
-     *             {...},
-     *         ]
-     *     </code>
-     * </pre>
+    /**
+     * <span style="color: red;">__Required__</span>
+     * <br><br>
+     * See IMenuItem <a href="/?path=/docs/components-navigation-header-api--page#imenuitem-api" target="_blank">API</a>
+     *  and <a href="/?path=/docs/components-navigation-header-api--page#imenuitem-format" target="_blank">expected format</a>.
      */
-    @Input() mainMenuData: IMenuItems[];
+    @Input() mainMenuData: IMenuItem[];
+    /** <span style="color: orange;">__Optional__</span> */
     @Input() reversed: boolean = false;
+    /**
+     * <span style="color: orange;">__Optional__</span>
+     * <br><br>
+     * Search field placeholder text
+     */
     @Input() searchPlaceholder: string = 'What are you looking for?';
     /**
      * Header's `data-theme` attribute
      * @ignore
      */
     @Input() theme: 'videotron' | 'business' = 'videotron';
-    /** Expected format:
-     * <pre>
-     *     <code>
-     *         [
-     *             {
-     *                 "activeParams": `IActiveMenuItemParams`,
-     *                 "class": `string`,
-     *                 "href": `string`,
-     *                 "isActive": `boolean`,
-     *                 "isActiveLinkClickable": `boolean`,
-     *                 "label": `string`,
-     *                 "rel": `string`,
-     *             },
-     *             {...},
-     *         ]
-     *     </code>
-     * </pre>
+    /**
+     * <span style="color: orange;">__Optional__</span>
+     * <br><br>
+     * See ITopBar <a href="/?path=/docs/components-navigation-header-api--page#itopbar-api" target="_blank">API</a>
+     *  and <a href="/?path=/docs/components-navigation-header-api--page#itopbar-format" target="_blank">expected format</a>.
      */
     @Input() topBarData: ITopBar;
 
