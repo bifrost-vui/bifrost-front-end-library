8e095b948a02eff3c5ceb699d6d9e11992d8b454
Maxim-Alexandre Lalancette <maxim-alexandre.lalancette@alithya.com>
2023-06-22 15:41:30 -0400

    DS-460: Remove files from "old" navigation header

--- projects/front-end-library/src/lib/components/__old-navigation-header/angular/header.module.ts
+++
@@ -1,9 +0,0 @@
-import { NgModule } from '@angular/core';
-import { OldHeaderComponent } from './header.component';
-
-@NgModule({
-    declarations: [OldHeaderComponent],
-    imports: [],
-    exports: [OldHeaderComponent],
-})
-export class OldHeaderModule {}
--- projects/front-end-library/src/lib/js/bifrost-components.js
+++ projects/front-end-library/src/lib/js/bifrost-components.js
@@ -15,6 +15,3 @@ require('../components/input-slider/js/index.js');
 require('../components/navigation-header/js/index.js');
 require('../components/navigation-header-business/js/index.js');
 require('../components/slideshow/js/index.js');
-
-// TODO (MAIN-NAV): To delete when new Navigation Header is completed
-require('../components/__old-navigation-header/js/index.js');
