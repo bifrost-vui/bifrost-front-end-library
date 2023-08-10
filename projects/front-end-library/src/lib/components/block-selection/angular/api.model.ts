d8263c6402015cfd6eb734ab4e8321f0b69cedd6
Maxim-Alexandre Lalancette <maxim-alexandre.lalancette@alithya.com>
2023-08-10 17:08:23 -0400

    Adjust Block Selection documentation - Adjust props order alphabetically - Add an internal API page - Add a lot of documentation for each props

---
+++ projects/front-end-library/src/lib/components/block-selection/angular/api.model.ts
@@ -0,0 +1,35 @@
+export interface IBlockSelection {
+    class?: string;
+    contentLists?: IContentLists[];
+    isCollapsableOnMobile?: boolean;
+    isCollapsedByDefaultOnMobile?: boolean;
+    title?: string;
+    titleTag?: string;
+    topLink?: ILink;
+    totalPrice?: object;
+}
+
+export interface IContentLists {
+    btnDelete?: ILink;
+    btnModify?: ILink;
+    description?: string;
+    fullPrice?: string;
+    link?: ILink;
+    price?: string;
+    priceInfo?: string;
+    saving?: string;
+    title?: string;
+}
+
+export interface ILink {
+    href: string;
+    label?: string;
+}
+
+export interface ITotalPrice {
+    details: string;
+    label: string;
+    price: string;
+    priceDetails: string;
+    priceInfo?: string;
+}
--- projects/front-end-library/src/lib/components/block-selection/angular/block-selection.component.ts
+++ projects/front-end-library/src/lib/components/block-selection/angular/block-selection.component.ts
@@ -1,28 +1,73 @@
 import { Component, ViewEncapsulation, OnInit, Input, Output, EventEmitter } from '@angular/core';
-
-/**
- * API is the same between **Angular** and **Drupal**.
- *
- */
+import { IBlockSelection, IContentLists, ILink, ITotalPrice } from './api.model';
 
 @Component({
     selector: 'bf-block-selection',
     templateUrl: './block-selection.component.html',
-    // styleUrls: ['../common/style.scss'],
 })
-export class BlockSelectionComponent implements OnInit {
+export class BlockSelectionComponent implements IBlockSelection, OnInit {
     constructor() {}
 
-    @Input() topLink: object;
-    @Input() contentLists: object;
-    @Input() totalPrice: object;
-    @Input() isCollapsableOnMobile: boolean;
-    @Input() isCollapsedByDefaultOnMobile: boolean;
-
-    @Input() title: string;
-    @Input() titleTag: string;
+    /**
+     * <span style="color: orange;">__Optional__</span>
+     * <br><br>
+     * Custom classes on the block main element.
+     */
     @Input() class: string;
-    @Input() reversed: boolean;
+    /**
+     * <span style="color: orange;">__Optional__</span>
+     * <br><br>
+     * - List of items content separated by a line.
+     * - See `IContentLists`
+     *    <a href="/?path=/docs/components-block-selection-api--page#icontentlists-api" target="_blank">API</a>
+     *    and <a href="/?path=/docs/components-block-selection-api--page#icontentlists-format" target="_blank">expected format</a>.
+     */
+    @Input() contentLists: IContentLists[];
+    /**
+     * <span style="color: orange;">__Optional__</span>
+     * <br><br>
+     * If `true`, on mobile resolution, the content will be collapsable.
+     */
+    @Input() isCollapsableOnMobile: boolean = false;
+    /**
+     * <span style="color: orange;">__Optional__</span>
+     * <br><br>
+     * - If `true`, on mobile resolution, the content will be collapsed by default.
+     * - Only works if `isCollapsableOnMobile` is `true`.
+     */
+    @Input() isCollapsedByDefaultOnMobile: boolean = false;
+    /**
+     * <span style="color: orange;">__Optional__</span>
+     * <br><br>
+     * - It displays at the top of the component.
+     * - If `isCollapsableOnMobile` is `true`, then it will be the button's label
+         on mobile resolution.
+     */
+    @Input() title: string;
+    /**
+     * <span style="color: orange;">__Optional__</span>
+     * <br><br>
+     * This is the HTML tag used for `title`.
+     */
+    @Input() titleTag: string = 'h1';
+    /**
+     * <span style="color: orange;">__Optional__</span>
+     * <br><br>
+     * It displays on the right of `title`.
+     * - See `ILink`
+     *    <a href="/?path=/docs/components-block-selection-api--page#ilink-api" target="_blank">API</a>
+     *    and <a href="/?path=/docs/components-block-selection-api--page#ilink-format" target="_blank">expected format</a>.
+     */
+    @Input() topLink: ILink;
+    /**
+     * <span style="color: orange;">__Optional__</span>
+     * <br><br>
+     * - It displays in the footer of the component.
+     * - See `ITotalPrice`
+     *    <a href="/?path=/docs/components-block-selection-api--page#itotalprice-api" target="_blank">API</a>
+     *    and <a href="/?path=/docs/components-block-selection-api--page#itotalprice-format" target="_blank">expected format</a>.
+     */
+    @Input() totalPrice: ITotalPrice;
 
     ngOnInit() {
         console.log('block selection', this);
