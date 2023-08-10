61f757ef66fb9f61f98076769cd78fa23210021c
Maxim-Alexandre Lalancette <maxim-alexandre.lalancette@alithya.com>
2023-08-10 17:23:28 -0400

    Adjust Hero component documentation

--- projects/front-end-library/src/lib/components/hero/angular/hero.component.ts
+++ projects/front-end-library/src/lib/components/hero/angular/hero.component.ts
@@ -1,10 +1,7 @@
 import { Component, ViewEncapsulation, OnInit, Input } from '@angular/core';
 import { IPrice } from '../../price/angular/api.model';
-
-/**
- * Description of the component Hero
- *
- */
+import { IBadge } from '../../badge/angular/api.model';
+import { IBlockSelection } from '../../block-selection/angular/api.model';
 
 @Component({
     selector: 'bf-hero',
@@ -14,19 +11,38 @@ import { IPrice } from '../../price/angular/api.model';
 export class HeroComponent implements OnInit {
     constructor() {}
 
-    @Input() background: 'ground' | 'underground' | 'highlight';
     /**
-     * You can pass directly the **label** as a **string** or an **object** based on
-     * [Badge](/?path=/docs/components-badge--drupal) component API.
+     * <span style="color: orange;">__Optional__</span>
+     * <br><br>
+     * Background color of the hero.
      */
-    @Input() badge: any;
+    @Input() background: 'ground' | 'underground' | 'highlight' = 'underground';
     /**
+     * <span style="color: orange;">__Optional__</span>
+     * <br><br>
+     * - This badge displays between the `breadcrumb` and the `upperTitle`.
+     * - See Badge Component [API](/?path=/docs/components-badge--drupal).
+     */
+    @Input() badge: IBadge;
+    /**
+     * <span style="color: orange;">__Optional__</span>
+     * <br><br>
      * - Does not work if there is an `image` and `image.isSplitted: true`.
-     * - Insert the component BlockSelection.
-     * - Explore its [Component API](/?path=/docs/components-block--drupal-selection).
+     * - It displays on the right side of the hero banner.
+     * - It uses the `Block Selection` component or you can pass
+         your own content with `blockSelection` Twig block.
+     * - If you use the `Block Selection` component,
+         see its [API](/?path=/docs/components-block-selection--drupal).
      */
-    @Input() blockSelection: object | 'TwigBlock';
-    @Input() breadcrumb: string;
+    @Input() blockSelection: IBlockSelection | 'TwigBlock';
+    /**
+     * <span style="color: orange;">__Optional__</span>
+     * <br><br>
+     * - It displays at the top of the component.
+     * - You can pass your own HTML code directly with the `breadcrumb` prop
+         or you can pass it through the `breadcrumb` Twig block.
+     */
+    @Input() breadcrumb: HTMLElement | 'TwigBlock';
     /**
      * - Insert buttons (max of 2 buttons).
      * - Explore its [Component API](/?path=/docs/components-button--drupal).
