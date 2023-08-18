204eabc377503a37d1495ddd9fc7b519d4c7eb52
Maxim-Alexandre Lalancette <maxim-alexandre.lalancette@alithya.com>
2023-08-18 15:53:03 -0400

    Overhaul of color system - Add a "reversed" CSS variable - Optimize colors SCSS file - Adjust documentation

--- projects/front-end-library/src/lib/_doc/styles/1-colors/colors.js
+++ projects/front-end-library/src/lib/_doc/styles/1-colors/colors.js
@@ -1,11 +1,17 @@
 const colors = [
     {
         name: 'Content',
-        description: 'Meant to be used on text and SVG.',
+        description: 'Meant to be used on text and SVG. CSS class will affect only the "color" property.',
         list: [
+            {
+                label: 'Brand',
+                name: 'brand',
+                reversed: false,
+            },
             {
                 label: 'Neutral Primary',
                 name: 'neutral-primary',
+                reversed: false,
             },
             {
                 label: 'Neutral Secondary',
@@ -18,17 +24,13 @@ const colors = [
             {
                 label: 'Highlight',
                 name: 'highlight',
-                hasUtilityClass: false,
-            },
-            {
-                label: 'Brand',
-                name: 'brand',
             },
         ],
     },
     {
         name: 'Background',
-        description: 'Meant to be used on non-interactive visual elements.',
+        description:
+            'Meant to be used on non-interactive visual elements. CSS class will affect only the "background-color" property.',
         list: [
             {
                 label: 'Ground',
@@ -41,30 +43,34 @@ const colors = [
             {
                 label: 'Highlight',
                 name: 'bg-highlight',
+                reversed: false,
             },
             {
                 label: 'Overlay black 20%',
                 name: 'bg-overlay-black-20prct',
-                hasUtilityClass: false,
+                reversed: false,
             },
             {
                 label: 'Overlay black 50%',
                 name: 'bg-overlay-black-50prct',
-                hasUtilityClass: false,
+                reversed: false,
             },
         ],
     },
     {
         name: 'Background Action',
-        description: 'Meant to be used on interactive visual elements.',
+        description:
+            'Meant to be used on interactive visual elements. CSS class will affect only the "background-color" property.',
         list: [
             {
                 label: 'Action primary',
                 name: 'bg-action-primary',
+                reversed: false,
             },
             {
                 label: 'Action primary hover',
                 name: 'bg-action-primary-hover',
+                reversed: false,
             },
             {
                 label: 'Action secondary',
@@ -91,57 +97,63 @@ const colors = [
     },
     {
         name: 'Stroke',
-        description: 'Meant to be used on borders.',
+        description: 'Meant to be used on borders. CSS class will affect only the "border-color" property.',
         list: [
             {
                 label: 'Primary',
                 name: 'stroke-primary',
                 hasUtilityClass: false,
-                UtilityClassName: 'bf-border-color-primary',
+                UtilityClassName: ['bf-border-color-primary'],
             },
             {
                 label: 'Secondary',
                 name: 'stroke-secondary',
                 hasUtilityClass: false,
-                UtilityClassName: 'bf-border-color-secondary',
+                UtilityClassName: ['bf-border-color-secondary'],
             },
             {
                 label: 'Tertiary',
                 name: 'stroke-tertiary',
                 hasUtilityClass: false,
-                UtilityClassName: 'bf-border-color-tertiary',
+                UtilityClassName: ['bf-border-color-tertiary'],
             },
             {
                 label: 'Highlight',
                 name: 'stroke-highlight',
                 hasUtilityClass: false,
-                UtilityClassName: 'bf-border-color-highlight',
+                UtilityClassName: ['bf-border-color-highlight'],
+                note: 'In Figma, it is named "Stroke active" instead of "Stroke Highlight".',
             },
         ],
     },
     {
         name: 'Feedback',
-        description: 'Convey meaning through color for visual support element',
+        description:
+            'Convey meaning through color for visual support element. CSS classes for text, background-color and border.',
         list: [
             {
                 label: 'Neutral',
                 name: 'feedback-neutral',
-                hasUtilityClass: false,
+                UtilityClassName: ['bf-color-bg-feedback-neutral', 'bf-border-feedback-neutral'],
+                reversed: false,
             },
             {
                 label: 'Success',
                 name: 'feedback-success',
-                hasUtilityClass: false,
+                UtilityClassName: ['bf-color-bg-feedback-success', 'bf-border-feedback-success'],
+                reversed: false,
             },
             {
                 label: 'Warning',
                 name: 'feedback-warning',
-                hasUtilityClass: false,
+                UtilityClassName: ['bf-color-bg-feedback-warning', 'bf-border-feedback-warning'],
+                reversed: false,
             },
             {
                 label: 'Negative',
                 name: 'feedback-negative',
-                hasUtilityClass: false,
+                UtilityClassName: ['bf-color-bg-feedback-negative', 'bf-border-feedback-negative'],
+                reversed: false,
             },
         ],
     },
