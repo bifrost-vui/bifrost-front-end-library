080faec1cb58c779abdd79e8013471f7e0bc9ca2
Maxim-Alexandre Lalancette <maxim-alexandre.lalancette@alithya.com>
2023-08-28 12:17:28 -0400

    Add custom colors for Card component - Add new "badgeCustomBgColor" prop for Badge custom color - Add new "badgeCustomFontColor" prop for Badge custom color - Add new "headerCustomBgColor" prop for card's header custom color - Add new "headerCustomFontColor" prop for card's header custom color - Adjust CSS to implement these header's custom colors - Adjust documentation - Adjust stories template

--- projects/front-end-library/src/lib/components/card/angular/card.component.ts
+++ projects/front-end-library/src/lib/components/card/angular/card.component.ts
@@ -23,6 +23,24 @@ export class CardComponent implements OnInit {
     /**
      * <span style="color: orange;">__Optional__</span>
      * <br><br>
+     * - It will change the `background-color` of the badge.
+     * - The string needs to be a color name or an hexadecimal color code (ex.: #F6F6F6).
+     * - In Storybook, there is an issue with the `#` character, so it breaks the rendering of the component.
+         To test this props, use a color name like `red`, `blue`, `yellow`, `black`, etc.
+     */
+    @Input() badgeCustomBgColor: string;
+    /**
+     * <span style="color: orange;">__Optional__</span>
+     * <br><br>
+     * - It will change the text `color` of the badge.
+     * - The string needs to be a color name or an hexadecimal color code (ex.: #F6F6F6).
+     * - In Storybook, there is an issue with the `#` character, so it breaks the rendering of the component.
+         To test this props, use a color name like `red`, `blue`, `yellow`, `black`, etc.
+     */
+    @Input() badgeCustomFontColor: string;
+    /**
+     * <span style="color: orange;">__Optional__</span>
+     * <br><br>
      * - Array of button(s). See Button Component [API](/?path=/docs/components-button--drupal#component-api)
      * - Or, extends with a block: `{% block buttons %} ... {% endblock %}`.
      */
@@ -87,6 +105,24 @@ export class CardComponent implements OnInit {
      * - Expected format: `["128 Go", "256 Go"]`
      */
     @Input() detailsStorage: string[];
+    /**
+     * <span style="color: orange;">__Optional__</span>
+     * <br><br>
+     * - It will change the `background-color` of the card's header.
+     * - The string needs to be a color name or an hexadecimal color code (ex.: #F6F6F6).
+     * - In Storybook, there is an issue with the `#` character, so it breaks the rendering of the component.
+         To test this props, use a color name like `red`, `blue`, `yellow`, `black`, etc.
+     */
+    @Input() headerCustomBgColor: string;
+    /**
+     * <span style="color: orange;">__Optional__</span>
+     * <br><br>
+     * - It will change the text `color` of the card's header.
+     * - The string needs to be a color name or an hexadecimal color code (ex.: #F6F6F6).
+     * - In Storybook, there is an issue with the `#` character, so it breaks the rendering of the component.
+         To test this props, use a color name like `red`, `blue`, `yellow`, `black`, etc.
+     */
+    @Input() headerCustomFontColor: string;
     /**
      * <span style="color: orange;">__Optional__</span>
      * <br><br>
