9804cf64108f984b572fc2e2fd36d83e917cfd63
Maxim-Alexandre Lalancette <maxim-alexandre.lalancette@alithya.com>
2023-08-28 14:51:05 -0400

    Adjust "badge icon" colors - Adjust bg-color and icon color when there is a custom header color

--- projects/front-end-library/src/lib/components/card/angular/card.component.ts
+++ projects/front-end-library/src/lib/components/card/angular/card.component.ts
@@ -24,6 +24,8 @@ export class CardComponent implements OnInit {
      * <span style="color: orange;">__Optional__</span>
      * <br><br>
      * - It will change the `background-color` of the badge.
+     * - If on __phone__ or __combo__ card type and `image.badgeIconName` has a value,
+         it will change the `background-color` of the `badge icon` in the lower right of the header.
      * - The string needs to be a color name or an hexadecimal color code (ex.: #F6F6F6).
      * - In Storybook, there is an issue with the `#` character, so it breaks the rendering of the component.
          To test this props, use a color name like `red`, `blue`, `yellow`, `black`, etc.
@@ -33,6 +35,8 @@ export class CardComponent implements OnInit {
      * <span style="color: orange;">__Optional__</span>
      * <br><br>
      * - It will change the text `color` of the badge.
+     * - If on __phone__ or __combo__ card type and `image.badgeIconName` has a value,
+         it will change the `color` of the `badge icon` in the lower right of the header.
      * - The string needs to be a color name or an hexadecimal color code (ex.: #F6F6F6).
      * - In Storybook, there is an issue with the `#` character, so it breaks the rendering of the component.
          To test this props, use a color name like `red`, `blue`, `yellow`, `black`, etc.
