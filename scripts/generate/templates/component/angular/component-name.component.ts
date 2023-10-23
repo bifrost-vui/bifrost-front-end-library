ded762074da4322bc9979d0af1952a022a1f8edb
Maxim-Alexandre Lalancette <maxim-alexandre.lalancette@alithya.com>
2023-10-23 16:52:54 -0400

    Adjust new component generator template files

--- scripts/generate/templates/component/angular/component-name.component.ts
+++ scripts/generate/templates/component/angular/component-name.component.ts
@@ -7,15 +7,28 @@ import { Component, OnInit, Input } from '@angular/core';
 export class %ComponentName%Component implements OnInit {
     constructor() {}
 
-    /**
-    * The inner text.
-    *
-    * @required
-    */
-    @Input() label: string;
+    /* In alphabetical order (remove this comment) */
 
-    /** Class override */
+    /**
+     * <span style="color: orange;">__Optional__</span>
+     * <br><br>
+     * Custom classes on %ComponentName% main element.
+     */
     @Input() class: string;
+    /**
+     * <span style="color: orange;">__Optional__</span>
+     * <br><br>
+     * %ComponentName% main element's `id` attribute.
+     */
+    @Input() id: string;
+    /**
+     * <span style="color: red;">__Required__</span>
+     * <br><br>
+     * The inner text.
+     *
+     * @required
+     */
+    @Input() label: string;
 
     ngOnInit() {
         console.log('%ComponentName%', this);
