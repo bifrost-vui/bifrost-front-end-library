1091b1c063f05439424451d4658b7fe2f4ff266e
Maxim-Alexandre Lalancette <maxim-alexandre.lalancette@alithya.com>
2023-11-15 14:27:56 -0500

    Adjust Input Checkbox component - Add an API Model file - Add documentation for API - Adjust `name` property on Input Checkbox - Adjust `name` property on Input Checkbox Option

--- projects/front-end-library/src/lib/components/input-checkbox-option/angular/input-checkbox-option.component.ts
+++ projects/front-end-library/src/lib/components/input-checkbox-option/angular/input-checkbox-option.component.ts
@@ -7,22 +7,130 @@ import { Component, OnInit, Input } from '@angular/core';
 export class InputCheckboxOptionComponent implements OnInit {
     constructor() {}
 
-    /** @required */
-    @Input() id: string;
-    @Input() name: string;
-    @Input() value: string;
-    @Input() isChecked: boolean;
-    @Input() label: HTMLElement;
-    @Input() labelClass: string;
-    @Input() labelExtraAttribute: string;
-    @Input() isRequired: boolean;
-    @Input() isDisabled: boolean;
-    @Input() isInvalid: boolean;
-    /** Needs to be defined if label is not defined. */
+    /**
+     * <span style="color: orange;">__Optional__</span>
+     * <br><br>
+     * - `aria-label` is an accessibility attribute.
+     * - Needs to be defined if `label` property is not defined.
+     * - It is used by a screen reader to tell the user what is the label of this checkbox.
+     */
     @Input() ariaLabel: string;
-
-    @Input() extraAttribute: string;
+    /**
+     * <span style="color: orange;">__Optional__</span>
+     * <br><br>
+     * Custom classes on component main element.
+     */
     @Input() class: string;
+    /**
+     * <span style="color: orange;">__Optional__</span>
+     * <br><br>
+     * - Extra attributes on the `<input>` element.
+     * - Can be a simple **string** or an **object**
+     *
+     * <br>
+     * _**String**_
+     *
+     * Must be between double-quotes ("), so use single-quotes (') for attributes.
+     * <pre>
+     *    <code>
+     *        "onclick='alert(\`lorem ipsum\`);' style='display: flex;'"
+     *    </code>
+     * </pre>
+     * <br><br>
+     * _**Object**_
+     * <pre>
+     *    <code>
+     *        {
+     *            "data-test-1": "test-1",
+     *            "data-test-2": "test-2"
+     *        }
+     *    </code>
+     * </pre>
+     */
+    @Input() extraAttribute: string | object;
+    /**
+     * <span style="color: red;">__Required__</span>
+     * <br><br>
+     * `<input>` element `id` attribute.
+     *
+     * @required
+     */
+    @Input() id: string;
+    /**
+     * <span style="color: orange;">__Optional__</span>
+     * <br><br>
+     * Is the checkbox item checked?
+     */
+    @Input() isChecked: boolean = false;
+    /**
+     * <span style="color: orange;">__Optional__</span>
+     * <br><br>
+     * Is the checkbox item disabled?
+     */
+    @Input() isDisabled: boolean = false;
+    /**
+     * <span style="color: orange;">__Optional__</span>
+     * <br><br>
+     * Is the checkbox item invalid (in error)?
+     */
+    @Input() isInvalid: boolean = false;
+    /**
+     * <span style="color: orange;">__Optional__</span>
+     * <br><br>
+     * Is the checkbox item required?
+     */
+    @Input() isRequired: boolean = false;
+    /**
+     * <span style="color: orange;">__Optional__</span>
+     * <br><br>
+     * Text on the right of the `<input>` element.
+     */
+    @Input() label: string | HTMLElement;
+    /**
+     * <span style="color: orange;">__Optional__</span>
+     * <br><br>
+     * Custom classes on `<label>` element.
+     */
+    @Input() labelClass: string;
+    /**
+     * <span style="color: orange;">__Optional__</span>
+     * <br><br>
+     * - Extra attributes on the `<span>` element that contains the label text.
+     * - Can be a simple **string** or an **object**
+     *
+     * <br>
+     * _**String**_
+     *
+     * Must be between double-quotes ("), so use single-quotes (') for attributes.
+     * <pre>
+     *    <code>
+     *        "style='color: blue;'"
+     *    </code>
+     * </pre>
+     * <br><br>
+     * _**Object**_
+     * <pre>
+     *    <code>
+     *        {
+     *            "data-test-1": "test-1",
+     *            "data-test-2": "test-2"
+     *        }
+     *    </code>
+     * </pre>
+     */
+    @Input() labelExtraAttribute: string | object;
+    /**
+     * <span style="color: orange;">__Optional__</span>
+     * <br><br>
+     * This value will be put in the `name` attribute on the `<input>` element.
+     */
+    @Input() name: string;
+    /**
+     * <span style="color: orange;">__Optional__</span>
+     * <br><br>
+     * This value will be put in the `value` attribute on the `<input>` element.
+     */
+    @Input() value: string;
 
     ngOnInit() {
         console.log('InputCheckboxOption', this);
